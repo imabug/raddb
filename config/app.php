77229100ac659b88f0bc5ccf91d1c4b77bb15f9a
Eugene Mah <eugenemah@gmail.com>
2016-09-26 08:21:01 -0400

    Setup consoletvs/charts package

--- config/app.php
+++ config/app.php
@@ -166,7 +166,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        ConsoleTVs\Charts\ChartsServiceProvider::class,
         //
 
         /*
@@ -224,6 +224,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Charts' => ConsoleTVs\Charts\Charts::class,
 
     ],
 
