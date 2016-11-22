35d051590bd25e8205dddc73747a0923ab80c0e4
imabug <eugenemah@gmail.com>
2016-11-22 09:51:12 -0500

    Make each location a link to the edit page instead of using a button

--- resources/views/admin/locations_index.blade.php
+++ resources/views/admin/locations_index.blade.php
@@ -4,19 +4,14 @@
 
 @section('content')
 <h2>Locations</h2>
-
-<table class="table"
+<p>Click a location to edit</p>
+<table class="table">
 	<tbody>
 @foreach ($locations->chunk(2) as $chunk )
 		<tr>
 		@foreach ($chunk as $location)
 			<td>{{ $location->id }}</td>
-			<td>{{ $location->location }}</td>
-			<td>
-				<form action="/admin/locations/{{ $location->id }}/edit" method="POST">
-					<button type="submit">Edit</button>
-				</form>
-			</td>
+			<td><a href="/admin/locations/{{ $location->id }}/edit">{{ $location->location }}</a></td>
 		@endforeach
 		</tr>
 @endforeach
@@ -25,10 +20,12 @@
 
 <h2>Add a Location</h2>
 <!-- Add a new location -->
-<form action="/admin/locations" method="POST">
+<form class="form-inline" action="/admin/locations" method="POST">
+	<div class="form-group">
 	{{ csrf_field() }}
-New Location: <input type="TEXT" name="location" size="20" />
-<button type="SUBMIT">Add location</button> / <a href="/">Main</a>
+<label for="location">New Location:</label> <input type="TEXT" class="form-control" id="location" name="location" size="20" placeholder="Location" />
+<button type="SUBMIT" class="btn btn-default">Add location</button> / <a href="/">Main</a>
+	</div>
 </form>
 
 @endsection
