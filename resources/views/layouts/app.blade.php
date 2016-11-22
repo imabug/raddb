66ef6588b7058df9e42c45206b89aef9ea837c6a
imabug <eugenemah@gmail.com>
2016-11-21 19:13:08 -0500

    Fix path to app.css file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<link href="css/app.css" rel="stylesheet" />
+	<link href="/css/app.css" rel="stylesheet" />
 <title>Radiological Equipment Database</title>
 </head>
 <body>
