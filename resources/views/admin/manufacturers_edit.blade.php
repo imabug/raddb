d7235571f523a1bae34a96b74508fefb2dfa4569
imabug <eugenemah@gmail.com>
2016-11-22 10:07:05 -0500

    Add some CSS

--- resources/views/admin/manufacturers_edit.blade.php
+++ resources/views/admin/manufacturers_edit.blade.php
@@ -5,11 +5,13 @@
 @section('content')
 <h2>Edit Manufacturer</h2>
 
-<form action="/admin/manufacturers/{{ $manufacturer->id }}" method="POST">
-	{{ csrf_field() }}
-	{{ method_field('PUT') }}
-Manufacturer: <input type="TEXT" name="manufacturer" size="20" value="{{ $manufacturer->manufacturer }}"/>
-<button type="SUBMIT">Edit manufacturer</button> / <a href="/">Main</a>
+<form class="form-inline" action="/admin/manufacturers/{{ $manufacturer->id }}" method="POST">
+	<div class="form-group">
+		{{ csrf_field() }}
+		{{ method_field('PUT') }}
+		<label for"manufacturer">Manufacturer:</label> <input class="form-control" type="TEXT" id="manufacturer" name="manufacturer" size="20" value="{{ $manufacturer->manufacturer }}" />
+		<button class="btn btn-default" type="SUBMIT">Edit manufacturer</button> / <a href="/">Main</a>
+	</div>
 </form>
 
 @endsection
