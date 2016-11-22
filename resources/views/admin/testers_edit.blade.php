2f5ddeb0da940535ab4a360860b274bed14b8ff1
imabug <eugenemah@gmail.com>
2016-11-22 10:39:44 -0500

    Add some CSS

--- resources/views/admin/testers_edit.blade.php
+++ resources/views/admin/testers_edit.blade.php
@@ -5,12 +5,14 @@
 @section('content')
 <h2>Edit Tester</h2>
 
-<form action="/admin/testers/{{ $tester->id }}" method="POST">
-	{{ csrf_field() }}
-	{{ method_field('PUT') }}
-<label>Tester:</label> <input type="TEXT" name="name" size="20" value="{{ $tester->name }}"/>
-<label>Initials:</label> <input type="text" name="initials" size="4" value="{{ $tester->initials }}" />
-<button type="SUBMIT">Edit tester</button> / <a href="/">Main</a>
+<form class="form-inline" action="/admin/testers/{{ $tester->id }}" method="POST">
+	<div class="form-group">
+		{{ csrf_field() }}
+		{{ method_field('PUT') }}
+		<label for="name">Tester:</label> <input type="TEXT" class="form-control" id="name" name="name" size="20" value="{{ $tester->name }}"/>
+		<label for="initials">Initials:</label> <input type="text" class="form-control" id="name" name="initials" size="4" value="{{ $tester->initials }}" />
+		<button class="btn btn-default" type="SUBMIT">Edit tester</button> / <a href="/">Main</a>
+	</div>
 </form>
 
 @endsection
