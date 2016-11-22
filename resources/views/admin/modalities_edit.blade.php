8294b57a6a0b5df04c0e98d4a711e3a9e1b6caba
imabug <eugenemah@gmail.com>
2016-11-22 10:30:11 -0500

    Add some CSS

--- resources/views/admin/modalities_edit.blade.php
+++ resources/views/admin/modalities_edit.blade.php
@@ -5,11 +5,13 @@
 @section('content')
 <h2>Edit Modality</h2>
 
-<form action="/admin/modalities/{{ $modality->id }}" method="POST">
-	{{ csrf_field() }}
-	{{ method_field('PUT') }}
-Modality: <input type="TEXT" name="modality" size="20" value="{{ $modality->modality }}"/>
-<button type="SUBMIT">Edit modality</button> / <a href="/">Main</a>
+<form class="form-inline" action="/admin/modalities/{{ $modality->id }}" method="POST">
+	<div class="form-group">
+		{{ csrf_field() }}
+		{{ method_field('PUT') }}
+		<label for="modality">Modality:</label> <input type="TEXT" class="form-control" id="modality" name="modality" size="20" value="{{ $modality->modality }}"/>
+		<button  class="btn btn-default"type="SUBMIT">Edit modality</button> / <a href="/">Main</a>
+	</div>
 </form>
 
 @endsection
