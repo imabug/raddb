a1c344edbe0679d715613a8178ddab20d5e65fc6
imabug <eugenemah@gmail.com>
2016-12-08 20:27:05 -0500

    Add some CSS

--- resources/views/machine/machines_create.blade.php
+++ resources/views/machine/machines_create.blade.php
@@ -5,53 +5,56 @@
 @section('content')
 <h2>Add a machine</h2>
 
-<form action="/machines" method="POST">
-	{{ csrf_field() }}
-	<p><label>Modality:</label>
-	<select name="modality" size="1">
-		<option>Select modality</option>
+<form class="form-inline" action="/machines" method="POST">
+	<div class="form-group">
 
-	@foreach ($modalities as $modality)
-	<option value="{{ $modality->id }}">{{ $modality->modality }}</option>
-	@endforeach
+		{{ csrf_field() }}
+		<p><label for="modality">Modality:</label>
+		<select id="modality" name="modality" size="1">
+			<option>Select modality</option>
 
-	</select></p>
-	<p><label>Description:</label>	<input type="text" name="description" size="40" /></p>
-	<p><label>Manufacturer:</label>
-	<select name="manufacturer" size="1">
-		<option>Select manufacturer</option>
+		@foreach ($modalities as $modality)
+		<option value="{{ $modality->id }}">{{ $modality->modality }}</option>
+		@endforeach
 
-	@foreach ($manufacturers as $manufacturer)
-	<option value="{{ $manufacturer->id }}">{{ $manufacturer->manufacturer }}</option>
-	@endforeach
+		</select></p>
+		<p><label for="description">Description:</label>	<input type="text" name="description" size="40" /></p>
+		<p><label for="manufacturer">Manufacturer:</label>
+		<select id="manufacturer" name="manufacturer" size="1">
+			<option>Select manufacturer</option>
 
-	</select></p>
-	<p><label>Model:</label> <input type="text" name="model" size="20" /></p>
-	<p><label>Serial Number:</label> <input type="text" name="serialNumber" size="20" /></p>
-	<p><label>Vendor site ID:</label> <input type="text" name="vendSiteID" size="20" /></p>
-	<p><label>Manufacture date:</label> <input type="text" name="manufDate" size="10" /> (YYYY-MM-DD)</p>
-	<p><label>Install date:</label> <input type="text" name="installDate" size="10" /> (YYYY-MM-DD)</p>
-	<p><label>Location:</label>
-	<select name="location" size="1">
-		<option>Select location</option>
+		@foreach ($manufacturers as $manufacturer)
+		<option value="{{ $manufacturer->id }}">{{ $manufacturer->manufacturer }}</option>
+		@endforeach
 
-	@foreach ($locations as $location)
-	<option value="{{ $location->id }}">{{ $location->location }}</option>
-	@endforeach
+		</select></p>
+		<p><label for="model">Model:</label> <input type="text" id="model" name="model" size="20" /></p>
+		<p><label for="serialNumber">Serial Number:</label> <input type="text" id="serialNumber" name="serialNumber" size="20" /></p>
+		<p><label for="vendSiteID">Vendor site ID:</label> <input type="text" id="vendSiteID" name="vendSiteID" size="20" /></p>
+		<p><label for="manufDate">Manufacture date:</label> <input type="text" id="manufDate" name="manufDate" size="10" /> (YYYY-MM-DD)</p>
+		<p><label for="installDate">Install date:</label> <input type="text" id="installDate" name="installDate" size="10" /> (YYYY-MM-DD)</p>
+		<p><label for="location">Location:</label>
+		<select id="location" name="location" size="1">
+			<option>Select location</option>
 
-	</select></p>
-	<p><label>Room:</label> <input type="text" name="room" size="20" /></p>
-	<p><label>Machine status:</label>
-	<select name="status" size="1">
-		<option>Select status</option>
-		<option value="Active" selected="selected">Active</option>
-		<option value="Inactive">Inactive</option>
-		<option value="Removed">Removed</option>
-	</select></p>
-	<p><label>Notes:</label><br /> <textarea name="notes" rows="3" cols="70">Enter any notes about the machine here</textarea></p>
+		@foreach ($locations as $location)
+		<option value="{{ $location->id }}">{{ $location->location }}</option>
+		@endforeach
 
-	<p><button type="SUBMIT">Add machine</button> / <a href="/">Main</a></p>
+		</select></p>
+		<p><label for="room">Room:</label> <input type="text" id="room" name="room" size="20" /></p>
+		<p><label for="status">Machine status:</label>
+		<select id="status" name="status" size="1">
+			<option>Select status</option>
+			<option value="Active" selected="selected">Active</option>
+			<option value="Inactive">Inactive</option>
+			<option value="Removed">Removed</option>
+		</select></p>
+		<p><label for="notes">Notes:</label><br /> <textarea id="notes" name="notes" rows="3" cols="70">Enter any notes about the machine here</textarea></p>
 
+		<p><button type="SUBMIT">Add machine</button> / <a href="/">Main</a></p>
+
+	</div>
 </form>
 
 
--- resources/views/tubes/tubes_create.blade.php
+++ resources/views/tubes/tubes_create.blade.php
@@ -5,49 +5,51 @@
 @section('content')
 <h2>Add a tube</h2>
 
-<form action="/tubes" method="POST">
-	{{ csrf_field() }}
-<p><label>Machine:</label>
-	<select name="machine" size="1">
-	<option>Select machine</option>
+<form class="form-inline" action="/tubes" method="POST">
+	<div class="form-group">
+		{{ csrf_field() }}
+	<p><label for="machine">Machine:</label>
+		<select id="machine" name="machine" size="1">
+		<option>Select machine</option>
 
-	@foreach ($machines as $machine)
-	<option value="{{ $machine->id}}"
-		@if ($machine->id == $machineID)
-		selected="selected"
-		@endif
-		>{{ $machine->description }}</option>
-	@endforeach
-	</select></p>
-<p><label>Housing Manufacturer:</label>
-	<select name="hsgManufID" size="1">
-	<option>Select manufacturer</option>
+		@foreach ($machines as $machine)
+		<option value="{{ $machine->id}}"
+			@if ($machine->id == $machineID)
+			selected="selected"
+			@endif
+			>{{ $machine->description }}</option>
+		@endforeach
+		</select></p>
+	<p><label for="hsgManufID">Housing Manufacturer:</label>
+		<select id="hsgManufID" name="hsgManufID" size="1">
+		<option>Select manufacturer</option>
 
-	@foreach ($manufacturers as $manufacturer)
-	<option value="{{ $manufacturer->id }}">{{ $manufacturer->manufacturer }}</option>
-	@endforeach
+		@foreach ($manufacturers as $manufacturer)
+		<option value="{{ $manufacturer->id }}">{{ $manufacturer->manufacturer }}</option>
+		@endforeach
 
-	</select></p>
-<p><label>Housing Model:</label> <input type="TEXT" name="hsgModel" size="20"></p>
-<p><label>Housing SN:</label> <input type="TEXT" name="hsgSN" size="20"></p>
-<p><label>Insert Manufacturer:</label>
-	<select name="insertManufID" size="1">
-	<option>Select manufacturer</option>
+		</select></p>
+	<p><label for="hsgModel">Housing Model:</label> <input type="TEXT" id="hsgModel" name="hsgModel" size="20"></p>
+	<p><label for="hsgSN">Housing SN:</label> <input type="TEXT" id="hsgSN" name="hsgSN" size="20"></p>
+	<p><label for="insertManufID">Insert Manufacturer:</label>
+		<select id="insertManufID" name="insertManufID" size="1">
+		<option>Select manufacturer</option>
 
-	@foreach ($manufacturers as $manufacturer)
-	<option value="{{ $manufacturer->id }}">{{ $manufacturer->manufacturer }}</option>
-	@endforeach
+		@foreach ($manufacturers as $manufacturer)
+		<option value="{{ $manufacturer->id }}">{{ $manufacturer->manufacturer }}</option>
+		@endforeach
 
-	</select></p>
-<p><label>Insert Model:</label> <input type="text" name="insertModel" size="20" /></p>
-<p><label>Insert SN:</label> <input type="text" name="insertSN" size="20" /></p>
-<p><label>Manufacture Date:</label> <input type="text" name="manufDate" size="10" /> (YYYY-MM-DD)</p>
-<p><label>Install date:</label> <input type="text" name="installDate" size="10" /> (YYYY-MM-DD)</p>
-<p><label>Focal spot size: Large:</label> <input type="text" name="lfs" size="4" />mm
-	<label>Medium:</label> <input type="text" name="mfs" size="4" />mm
-	<label>Small:</label> <input type="text" name="sfs" size="4" />mm</p>
-<p><label>Notes</label><br /><textarea name="notes" rows="3" cols="70">Additional notes about this tube</textarea></p>
-<p><button type="SUBMIT">Add tube</button> / <a href="/">Main</a></p>
+		</select></p>
+	<p><label for="insertModel">Insert Model:</label> <input type="text" id="insertModel" name="insertModel" size="20" /></p>
+	<p><label for="insertSN">Insert SN:</label> <input type="text" id="insertSN" name="insertSN" size="20" /></p>
+	<p><label for="manufDate">Manufacture Date:</label> <input type="text" id="manufDate" name="manufDate" size="10" /> (YYYY-MM-DD)</p>
+	<p><label for="installDate">Install date:</label> <input type="text" id="installDate" name="installDate" size="10" /> (YYYY-MM-DD)</p>
+	<p><label for="lfs">Focal spot size: Large:</label> <input type="text" id="lfs" name="lfs" size="4" />mm
+		<label for="mfs">Medium:</label> <input type="text" id="mfs" name="mfs" size="4" />mm
+		<label for="sfs">Small:</label> <input type="text" id="sfs" name="sfs" size="4" />mm</p>
+	<p><label for="notes">Notes</label><br /><textarea id="notes" name="notes" rows="3" cols="70">Additional notes about this tube</textarea></p>
+	<p><button type="SUBMIT">Add tube</button> / <a href="/">Main</a></p>
+	</div>
 </form>
 
 @endsection
