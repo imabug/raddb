e6bbbddc636077a84670fc68f319921ce3405ce1
Eugene Mah <eugenemah@gmail.com>
2016-12-19 13:35:47 -0500

    Work on resolving recommendations

--- app/Http/Controllers/RecommendationController.php
+++ app/Http/Controllers/RecommendationController.php
@@ -2,6 +2,7 @@
 namespace RadDB\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use RadDB\TestDate;
 use RadDB\Recommendation;
 use RadDB\Http\Requests;
@@ -42,23 +43,25 @@ class RecommendationController extends Controller
 
     /**
      * Display the recommendations for a specific survey.
+     * URI: /recommendations/$surveyId
+     * Method: GET
      *
-     * @param int $id
+     * @param int $surveyId
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($surveyId)
     {
         // Get the machine description corresponding to the survey ID provided
         $machineDesc = TestDate::select('description')
             ->join('machines', 'testdates.machine_id', '=', 'machines.id')
-            ->where('testdates.id', $id)
+            ->where('testdates.id', $surveyId)
             ->first();
 
         // Get the recommendations
-        $recs = Recommendation::where('survey_id', $id)->get();
+        $recs = Recommendation::where('survey_id', $surveyId)->get();
 
         return view('surveys.recommendations', [
-            'surveyID' => $id,
+            'surveyID' => $surveyId,
             'machineDesc' => $machineDesc,
             'recs' => $recs
         ]);
@@ -76,15 +79,50 @@ class RecommendationController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     * Update the recommendations for $surveyID.
+     * URI: /recommendations/$surveyID
+     * Method: PUT
      *
      * @param \Illuminate\Http\Request $request
      * @param int $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $surveyID)
     {
-        //
+        $this->validate($request, [
+            'recID' => 'array',
+            'WONum' => 'string|max:20',
+            'RecResolveDate' => 'required|date_format:Y-m-d|max:10',
+            'ResolvedBy' => 'string|max:10',
+            'ServiceReport' => 'file|mimes:pdf',
+        ]);
+        $resolved = $request->recID;
+        $recResolveDate = $request->RecResolveDate;
+
+        // If a service report was uploaded, handle it
+        // Service reports are stored in the storage/app/public/ServiceReports/$recResolveYr
+        $path = "ServiceReports/" . date_parse($recResolveDate)['year'];
+        if (!is_dir($path)) {
+            Storage::makeDirectory($path);
+        }
+        if ($request->hasFile('ServiceReport')) {
+            $serviceReportFile = $request->ServiceReport;
+            $serviceReportPath = $serviceReportFile->storeAs($path, $serviceReportFile)
+        }
+
+        foreach ($resolved as $recId) {
+            // Retrieve the Recommendations
+            $recommendation = Recommendation::findOrFail($recId);
+
+            if (isset($request->WONum)) $recommendation->wo_number = $request->WONum;
+            if (isset($request->RecResolveDate)) $recommendation->rec_resolve_date = $recResolveDate;
+            if (isset($request->ResolvedBy)) $recommendation->resolved_by = $request->ResolvedBy;
+            $recommendation->resolved = 1;
+            $recommendation->rec_status = "Complete";
+            $recommendation->service_report_path = $serviceReportPath;
+        }
+
+        return redirect('/recommendations/' . $surveyID);
     }
 
     /**
