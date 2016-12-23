f93351d566f3caa83ce5ed71e5e9a3c2c8c4e6c6
Eugene Mah <eugenemah@gmail.com>
2016-12-23 10:39:29 -0500

    Create a footer module for the base template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,12 +15,6 @@
 @include('layouts.sidebar')
         @yield('content')
     </div>
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="/js/vendor/jquery.min.js"><\/script>')</script>
-<script src="/js/bootstrap.min.js"></script>
-
+@include('layouts.footer')
 </body>
 </html>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<p class="text-center"><small>Built with <a href="https://www.laravel.com" target="_blank">Laravel</a> and <a href="http://php.net" target="_blank">PHP</a>. Uses <a href="http://getbootstrap.com/" target="_blank">Bootstrap</a> and <a href="http://glyphicons.com/" target="_blank">Glyphicons</a></small></p>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="/js/vendor/jquery.min.js"><\/script>')</script>
+<script src="/js/bootstrap.min.js"></script>
