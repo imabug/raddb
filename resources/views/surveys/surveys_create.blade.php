549b10953f2ae64941b1c4755ea9a742f658b0c5
Eugene Mah <eugenemah@gmail.com>
2016-12-23 13:01:14 -0500

    Add CSS styling

--- resources/views/surveys/surveys_create.blade.php
+++ resources/views/surveys/surveys_create.blade.php
@@ -8,14 +8,14 @@
     for {{ $machines->description }}
 @endif
 </h2>
-<form action="/surveys" method="POST">
+<form class="form-inline" action="/surveys" method="POST">
 {{ csrf_field() }}
-<p><label>Machine:</label>
+<p><label for="machineID">Machine:</label>
 @if (isset($id))
 {{ $machines->description }}
-<input type="hidden" name="machineID" value="{{ $machines->id }}" />
+<input class="form-control" type="hidden" id="machineID" name="machineID" value="{{ $machines->id }}" />
 @else
-    <select name="machineID" size="1">
+    <select class="form-control" name="machineID" size="1">
     <option>Select a machine</option>
     @foreach ($machines as $machine)
     <option value="{{ $machine->id }}">{{ $machine->description }}</option>
@@ -23,10 +23,10 @@
     </select>
 @endif
 </p>
-<p><label>Date tested (YYYY-MM-DD):</label> <input type="text" name="test_date" size="10" /> <input type="checkbox" name="today" />Today</p>
+<p><label for="test_date">Date tested:</label> <input class="form-control" type="text" id="test_date" name="test_date" size="10" placeholder="YYYY-MM-DD" /> <input type="checkbox" name="today" />Today</p>
 <p>
-    <label>Tested by: </label>
-    <select name="tester1ID" size="1">
+    <label for="tester1ID">Tested by: </label>
+    <select class="form-control" id="tester1ID" name="tester1ID" size="1">
     <option value="0"></option
     @foreach ($testers as $tester)
     <option value="{{ $tester->id }}">{{ $tester->initials }}</option>
@@ -34,8 +34,8 @@
     </select>
 </p>
 <p>
-    <label>Tested by: </label>
-    <select name="tester2ID" size="1">
+    <label for="tester2ID">Tested by: </label>
+    <select class="form-control" id="tester2ID" name="tester2ID" size="1">
     <option value="0"></option
     @foreach ($testers as $tester)
     <option value="{{ $tester->id }}">{{ $tester->initials }}</option>
@@ -43,15 +43,15 @@
     </select>
 </p>
 <p>
-    <label>Type of test:</label>
-    <select name="test_type" size="1">
+    <label for="test_type">Type of test:</label>
+    <select class="form-control" id="test_type" name="test_type" size="1">
     @foreach ($testtypes as $testtype)
     <option value="{{ $testtype->id }}">{{ $testtype->test_type }}</option>
     @endforeach
     </select>
 </p>
-<p><label>Notes:</label> <textarea name="notes" rows="3" cols="70">Enter any notes about the survey</textarea></p>
-<p><label>Accession:</label> <input type="text" name="accession" size="12" /></p>
+<p><label for="notes">Notes:</label> <textarea class="form-control" id="notes" name="notes" rows="3" cols="70" placeholder="Enter any notes about the survey"></textarea></p>
+<p><label for="accession">Accession:</label> <input class="form-control" type="text" id="accession" name="accession" size="12" /></p>
 
 <p><button type="SUBMIT">Add survey</button> / <a href="/">Main</a></p>
 </form>
--- resources/views/surveys/surveys_edit.blade.php
+++ resources/views/surveys/surveys_edit.blade.php
@@ -4,17 +4,17 @@
 
 @section('content')
 <h2>Edit a survey for {{ $machine->description }}</h2>
-<form action="/surveys" method="POST">
+<form class="form-inline" action="/surveys" method="POST">
 {{ csrf_field() }}
 {{ method_field('PUT') }}
-<input type="hidden" name="id" value="{{ $survey->id }}" />
-<input type="hidden" name="machineID" value="{{ $machines->id }}" />
-<p><label>Machine:</label> {{ $machine->description }}
+<input class="form-control" type="hidden" id="id" name="id" value="{{ $survey->id }}" />
+<input class="form-control" type="hidden" id="machineID" name="machineID" value="{{ $machines->id }}" />
+<p>Machine: {{ $machine->description }}
 </p>
-<p><label>Date tested (YYYY-MM-DD):</label> <input type="text" name="test_date" size="10" value="{{ $survey->test_date }}" /></p>
+<p><label for="test_date">Date tested:</label> <input class="form-control" type="text" id="test_date" name="test_date" size="10" value="{{ $survey->test_date }}" placeholder="YYYY-MM-DD" /></p>
 <p>
-    <label>Tested by: </label>
-    <select name="tester1ID" size="1">
+    <label for="tester1ID">Tested by: </label>
+    <select class="form-control" id="tester1ID" name="tester1ID" size="1">
     <option></option
     @foreach ($testers as $t)
     <option value="{{ $t->id }}"
@@ -26,8 +26,8 @@
     </select>
 </p>
 <p>
-    <label>Tested by: </label>
-    <select name="tester2ID" size="1">
+    <label for="tester2ID">Tested by: </label>
+    <select class="form-control" id="tester2ID" name="tester2ID" size="1">
     <option></option
     @foreach ($testers as $t)
     <option value="{{ $t->id }}"
@@ -39,8 +39,8 @@
     </select>
 </p>
 <p>
-    <label>Type of test:</label>
-    <select name="test_type" size="1">
+    <label for="test_type">Type of test:</label>
+    <select class="form-control" id="test_type" name="test_type" size="1">
     @foreach ($testtypes as $tt)
     <option value="{{ $tt->id }}"
     @if ($tt->id == $testtype->id)
@@ -50,8 +50,8 @@
     @endforeach
     </select>
 </p>
-<p><label>Notes:</label> <textarea name="notes" rows="3" cols="70">{{ $survey->notes }}</textarea></p>
-<p><label>Accession:</label> <input type="text" name="accession" size="12" value="{{ $survey->accession }}" /></p>
+<p><label for="notes">Notes:</label> <textarea class="form-control" id="notes" name="notes" rows="3" cols="70">{{ $survey->notes }}</textarea></p>
+<p><label for="accession">Accession:</label> <input class="form-control" type="text" id="accession" name="accession" size="12" value="{{ $survey->accession }}" /></p>
 
 <p><button type="SUBMIT">Edit survey</button> / <a href="/">Main</a></p>
 </form>
