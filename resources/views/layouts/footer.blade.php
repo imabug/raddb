9027a71ab61f3d31ef5c97f920f129098bb4824d
Eugene Mah <eugenemah@gmail.com>
2016-12-28 11:48:18 -0500

    leave out the app.js file. was messing things up

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -6,4 +6,4 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="/js/vendor/jquery.min.js"><\/script>')</script>
 <script src="/js/bootstrap.min.js"></script>
-<script src="/js/app.js"></script>
+{{-- <script src="/js/app.js"></script> --}}
