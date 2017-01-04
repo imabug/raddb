d5b934dabbce2178d78c594fc7ee2eee42c0da93
imabug <eugenemah@gmail.com>
2017-01-03 20:28:02 -0500

    HTML5-ify things

--- resources/views/surveys/surveys_addReport.blade.php
+++ resources/views/surveys/surveys_addReport.blade.php
@@ -9,8 +9,8 @@
     <div class="form-group">
         {{ csrf_field() }}
         {{ method_field('PUT') }}
-        <p><label for="surveyId">Survey ID: </label> <input class="form-control" type="text" id="surveyId" name="surveyId" /></p>
-        <p><label for="surveyReport">Upload survey report: </label> <input class="form-control" type="file" id="surveyReport" name="surveyReport" /></p>
+        <p><label for="surveyId">Survey ID: </label> <input class="form-control" type="text" id="surveyId" name="surveyId"></p>
+        <p><label for="surveyReport">Upload survey report: </label> <input class="form-control" type="file" id="surveyReport" name="surveyReport" ></p>
         <p><button type="submit">Submit survey report</button> / <a href="/">Main</a></p>
     </div>
 </form>
