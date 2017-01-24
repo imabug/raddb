31dd582a9a7e5ecd0d1d7ac03d703b24e51a5076
imabug <eugenemah@gmail.com>
2017-01-23 19:11:55 -0500

    Add ConsoleTVs\Charts package

--- config/app.php
+++ config/app.php
@@ -224,7 +224,7 @@ return [
         'URL'          => Illuminate\Support\Facades\URL::class,
         'Validator'    => Illuminate\Support\Facades\Validator::class,
         'View'         => Illuminate\Support\Facades\View::class,
-        'Charts'       => ConsoleTVs\Charts\Charts::class,
+        'Charts'       => ConsoleTVs\Charts\Facades\Charts::class,
 
     ],
 
