38fc155dfdf0cf8ed3c8fe61a6a30804f6b02940
Eugene Mah <eugenemah@gmail.com>
2017-01-24 13:07:30 -0500

    switch to policy authorization

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -32,7 +32,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->registerPolicies($gate);
+        $this->registerPolicies();
 
     }
 }
