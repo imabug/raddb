e75e45ff07b697dfca522cb9d56a7612cda97dd0
Eugene Mah <eugenemah@gmail.com>
2017-01-27 13:17:36 -0500

    Link in the latest version of jQuery

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,7 +3,6 @@
 <!-- Bootstrap core JavaScript
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="/js/vendor/jquery.min.js"><\/script>')</script>
 <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-{{-- <script src="/js/app.js"></script> --}}
