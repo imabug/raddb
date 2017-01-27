9e130e6d5a74a08b6a809e3db93a889b453ea87e
Eugene Mah <eugenemah@gmail.com>
2017-01-27 13:02:36 -0500

    Use blade includes for dashboard pages to minimize repetition

---
+++ resources/views/inc/pending.blade.php
@@ -0,0 +1,19 @@
+<h2><span class="label label-default">Pending surveys</span></h2>
+<table class="table table-striped table-hover">
+	<thead>
+		<tr>
+			<th>Survey ID</th><th>Description</th><th>Date Scheduled</th><th>Accession</th><th>Survey Note</th>
+		</tr>
+	</thead>
+	<tbody>
+@foreach ($pendingSurveys as $pending)
+		<tr>
+			<td>{{ $pending->surveyId }}</td>
+			<td><a href="{{ route('machines.show', $pending->machineId) }}">{{ $pending->description }}</a></td>
+			<td>{{ $pending->test_date}}</td>
+			<td>{{ $pending->accession }}</td>
+			<td>{{ $pending->notes }}</td>
+		</tr>
+@endforeach
+	</tbody>
+</table>
---
+++ resources/views/inc/survey_schedule.blade.php
@@ -0,0 +1,31 @@
+<h2><span class="label label-default">Survey Schedule</span></h2>
+
+<table class="table table-striped table-hover">
+	<thead>
+		<tr>
+			<th>ID</th><th>Description</th><th>Previous</th><th>Prev SurveyID</th><th>Current</th><th>Curr SurveyID</th><th>Recs</th><th>Recs Resolved</th>
+		</tr>
+	</thead>
+	<tbody>
+@foreach ($surveySchedule as $ss)
+		<tr>
+			<td>{{ $ss->id }}</td>
+			<td><a href="{{ route('machines.show', $ss->id) }}">{{ $ss->description }}</a></td>
+            <td><a href="{{ route('recommendations.show', $ss->prevSurveyID)}}" title="Recommendations" alt="Recommendations">{{ $ss->prevSurveyDate }}</a></td>
+		@if (empty($ss->prevSurveyReport))
+            <td>{{ $ss->prevSurveyID }}</td>
+		@else
+            <td><a href="{{ route('reports.show', ["survey", $ss->prevSurveyID]) }}" target="_blank" title="Survey report" alt="Survey report">{{ $ss->prevSurveyID }}</a></td>
+		@endif
+        <td><a href="{{ route('recommendations.show', $ss->currSurveyID)}}" title="Recommendations" alt="Recommendations">{{ $ss->currSurveyDate }}</a></td>
+		@if (empty($ss->currSurveyReport))
+            <td>{{ $ss->currSurveyID }}</td>
+		@else
+			<td><a href="{{ route('reports.show', ["survey", $ss->currSurveyID]) }}" target="_blank" title="Survey report" alt="Survey report">{{ $ss->currSurveyID}}</a></td>
+		@endif
+			<td></td>
+			<td></td>
+		</tr>
+@endforeach
+	</tbody>
+</table>
