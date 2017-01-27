416df3e7c64e8622b8cb80302e8280b87393329b
Eugene Mah <eugenemah@gmail.com>
2017-01-27 08:49:20 -0500

    Form formatting

--- resources/views/opnotes/opnote_create.blade.php
+++ resources/views/opnotes/opnote_create.blade.php
@@ -21,18 +21,20 @@
 <form class="form-inline" action="{{ route('opnotes.store')}}" method="post">
     <div class="form-group">
         {{ csrf_field() }}
+        <p>
         @if($machines->count() > 1)
         <label for="machineId">Select a machine: </label>
         <select class="form-control" name="machineId">
         @foreach ($machines as $machine)
-            <option value="{{ $machine->id }}"></option>
+            <option value="{{ $machine->id }}">{{ $machine->description }}</option>
         @endforeach
         </select>
         @else
         <input type="hidden" name="machineId" value="$machine->id">
         @endif
-        <textarea name="note" rows="8" cols="80" placeholder="Enter operational note"></textarea>
-        <button type="submit" name="submit">Submit</button>
+        </p>
+        <p><textarea name="note" rows="8" cols="80" placeholder="Enter operational note"></textarea></p>
+        <p><button type="submit" name="submit">Submit</button></p>
     </div>
 </form>
 @endsection
