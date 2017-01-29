223c37f0838fedb781870ac801fdfc7ff54a7e00
imabug <eugenemah@gmail.com>
2017-01-29 10:37:30 -0500

    Use form request to validate recommendation updates

---
+++ app/Http/Requests/UpdateRecommendationRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace RadDB\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRecommendationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'recID'          => 'array',
+            'WONum'          => 'string|max:20',
+            'RecResolveDate' => 'required|date_format:Y-m-d|max:10',
+            'ResolvedBy'     => 'string|max:10',
+            'ServiceReport'  => 'file|mimes:pdf',
+        ];
+    }
+}
