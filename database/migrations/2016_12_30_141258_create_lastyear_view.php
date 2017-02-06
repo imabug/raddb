bbb4f159774e9e0712c3fddbfbb76a48a65184c5
Eugene Mah <eugenemah@gmail.com>
2017-02-06 14:56:04 -0500

    Add count of recommendations for each survey

--- database/migrations/2016_12_30_141258_create_lastyear_view.php
+++ database/migrations/2016_12_30_141258_create_lastyear_view.php
@@ -6,23 +6,19 @@ class CreateLastyearView extends Migration
 {
     /**
      * Create a view containing surveys from the previous year
-     * CREATE VIEW lastyear_view (machine_id,survey_id,test_date) AS
-     * SELECT testdates.machine_id,testdates.id,testdates.test_date
-     * FROM testdates
-     * WHERE testdates.test_date BETWEEN MAKEDATE(YEAR(CURDATE())-1,1) AND MAKEDATE(YEAR(CURDATE()),1)
-     * AND (testdates.type_id=1 or testdates.type_id=2)
-     * ORDER BY testdates.test_date DESC.
      *
      * @return void
      */
     public function up()
     {
         DB::statement('
-        CREATE VIEW lastyear_view (machine_id,survey_id,test_date,report_file_path) AS
-        SELECT machine_id,id as survey_id,test_date,report_file_path
+        CREATE VIEW lastyear_view (machine_id,survey_id,test_date,report_file_path,recCount) AS
+        SELECT machine_id,id as survey_id,test_date,report_file_path,count(recommendations.recommendation) AS recCount
         FROM testdates
+        LEFT JOIN recommendations on testdates.id=recommendations.survey_id
         WHERE testdates.test_date BETWEEN MAKEDATE(YEAR(CURDATE())-1,1) AND MAKEDATE(YEAR(CURDATE()),1)
         AND (testdates.type_id=1 or testdates.type_id=2)
+        GROUP BY testdates.id
         ORDER BY testdates.test_date ASC
         ');
     }
--- database/migrations/2016_12_30_141318_create_thisyear_view.php
+++ database/migrations/2016_12_30_141318_create_thisyear_view.php
@@ -6,23 +6,19 @@ class CreateThisyearView extends Migration
 {
     /**
      * Create a view containing surveys from the previous year
-     * CREATE VIEW thisyear_view (machine_id,survey_id,test_date) AS
-     * SELECT testdates.machine_id,testdates.id,testdates.test_date
-     * FROM testdates
-     * WHERE testdates.test_date BETWEEN MAKEDATE(YEAR(CURDATE()),1) AND MAKEDATE(YEAR(CURDATE())+1,1)
-     * and (testdates.type_id=1 or testdates.type_id=2)
-     * ORDER BY testdates.test_date ASC.
      *
      * @return void
      */
     public function up()
     {
         DB::statement('
-        CREATE VIEW thisyear_view (machine_id,survey_id,test_date,report_file_path) AS
-        SELECT machine_id,id as survey_id,test_date,report_file_path
+        CREATE VIEW thisyear_view (machine_id,survey_id,test_date,report_file_path,recCount) AS
+        SELECT machine_id,id as survey_id,test_date,report_file_path,count(recommendations.recommendation) as recCount
         FROM testdates
+        LEFT JOIN recommendations ON testdates.id=recommendations.survey_id
         WHERE testdates.test_date BETWEEN MAKEDATE(YEAR(CURDATE()),1) AND MAKEDATE(YEAR(CURDATE())+1,1)
         AND (testdates.type_id=1 or testdates.type_id=2)
+        GROUP BY testdates.id
         ORDER BY testdates.test_date ASC
         ');
     }
