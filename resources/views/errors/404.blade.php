aa480809453dffe811a7731b8c6cdb54e029188e
Eugene Mah <eugenemah@gmail.com>
2017-02-09 10:39:48 -0500

    Add HTTP 404 error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+<!-- resources/views/errors/404.blade.php -->
+
+@extends('layouts.app')
+@section('content')
+        <div class="content">
+            <h2>Page not found</h2>
+            <p class="bg-warning">The request page was not found here.</p>
+        </div>
+@endsection
