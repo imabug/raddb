f92ac27def9ca61b57404abf2c5c4cbaa5af2633
Eugene Mah <eugenemah@gmail.com>
2017-02-17 11:08:54 -0500

    Make Help a proper dropdown
    Forgot to add the correct class tags to make the Help menu a dropdown

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -70,11 +70,13 @@
             @else
             <li><a href="{{route('home.index')}}">Login</a></li>
             @endif
-            <li>Help</li>
-            <ul>
-                <li><a href="http://128.23.56.214/MPwiki/index.php/RadDB">Help (local)</a></li>
-                <li><a href="https://github.com/imabug/raddb/wiki">Help (on Github)</a></li>
-            </ul>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Help<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="http://128.23.56.214/MPwiki/index.php/RadDB" target="_blank">Help (local)</a></li>
+                    <li><a href="https://github.com/imabug/raddb/wiki" target="_blank">Help (on Github)</a></li>
+                </ul>
+            </li>
         </ul>
     </div>
 </nav>
