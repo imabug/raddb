d0f3a1ed232ab7d9ec8c933d0904894a8761f813
Eugene Mah <eugenemah@gmail.com>
2017-02-21 08:42:25 -0500

    Add default values for focal spot sizes

--- resources/views/tubes/tubes_create.blade.php
+++ resources/views/tubes/tubes_create.blade.php
@@ -44,9 +44,9 @@
 	<p><label for="insertSN">Insert SN:</label> <input class="form-control" type="text" id="insertSN" name="insertSN" size="20" ></p>
 	<p><label for="manufDate">Manufacture Date:</label> <input class="form-control" type="date" id="manufDate" name="manufDate" size="10" ></p>
 	<p><label for="installDate">Install date:</label> <input class="form-control" type="date" id="installDate" name="installDate" size="10" ></p>
-	<p><label for="lfs">Focal spot size: Large:</label> <input class="form-control" type="text" id="lfs" name="lfs" size="4" />mm
-    	<label for="mfs">Medium:</label> <input class="form-control" type="text" id="mfs" name="mfs" size="4" >mm
-		<label for="sfs">Small:</label> <input class="form-control" type="text" id="sfs" name="sfs" size="4" >mm</p>
+	<p><label for="lfs">Focal spot size: Large:</label> <input class="form-control" type="text" id="lfs" name="lfs" size="4" value="0">mm
+    	<label for="mfs">Medium:</label> <input class="form-control" type="text" id="mfs" name="mfs" size="4" value="0">mm
+		<label for="sfs">Small:</label> <input class="form-control" type="text" id="sfs" name="sfs" size="4" value="0">mm</p>
 	<p><label for="notes">Notes</label><br /><textarea class="form-control" id="notes" name="notes" rows="3" cols="70" placeholder="Additional notes about this tube"></textarea></p>
 	<p><button class="form-control" type="SUBMIT">Add tube</button></p>
 	</div>
