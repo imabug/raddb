b7995687ccc6446117433229507660412c1ed730
Eugene Mah <eugenemah@gmail.com>
2017-02-21 10:53:27 -0500

    Add database seeders
    Seed the modalities and testtypes tables

---
+++ database/seeds/ModalitiesTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class ModalitiesTableSeeder extends Seeder
+{
+    /**
+     * Seed the modalities table.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('modalities')->insert([
+            [ 'modality' => 'Angiography' ],
+            [ 'modality' => 'Bone Density' ],
+            [ 'modality' => 'C-Arm' ],
+            [ 'modality' => 'Cath Lab' ],
+            [ 'modality' => 'CR Reader' ],
+            [ 'modality' => 'CR Workstation' ],
+            [ 'modality' => 'CT' ],
+            [ 'modality' => 'Dental' ],
+            [ 'modality' => 'Fluoroscopy' ],
+            [ 'modality' => 'Mammography' ],
+            [ 'modality' => 'Mammography Workstation' ],
+            [ 'modality' => 'MRI' ],
+            [ 'modality' => 'Nuclear Medicine' ],
+            [ 'modality' => 'Portable' ],
+            [ 'modality' => 'Printer' ],
+            [ 'modality' => 'Rad/Fluoro' ],
+            [ 'modality' => 'Radiography' ],
+            [ 'modality' => 'Stereotactic Breast Biopsy' ],
+            [ 'modality' => 'Test Equipment' ],
+            [ 'modality' => 'Ultrasound' ],
+        ]);
+    }
+}
---
+++ database/seeds/TestTypesTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class TestTypesTableSeeder extends Seeder
+{
+    /**
+     * Seed the testtypes table.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('modalities')->insert([
+            [ 'test_type' => 'Routine compliance' ],
+            [ 'test_type' => 'Acceptance' ],
+            [ 'test_type' => 'Major service check' ],
+            [ 'test_type' => 'Follow up' ],
+            [ 'test_type' => 'Phantom review' ],
+            [ 'test_type' => 'Shielding survey' ],
+            [ 'test_type' => 'Bone densitometer survey' ],
+            [ 'test_type' => 'Other' ],
+            [ 'test_type' => 'Accreditation survey' ],
+            [ 'test_type' => 'Calibration' ],
+        ]);
+    }
+}
