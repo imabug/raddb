3dc601ae82b9094b6cf8a409e1ee633cc15d5d0c
Eugene Mah <eugenemah@gmail.com>
2017-03-16 13:20:37 -0400

    Add some divs to the machine info panel.
    Left col-md-4 is for the machine info.
    Right col-md-8 will be for machine images

--- resources/views/machine/detail.blade.php
+++ resources/views/machine/detail.blade.php
@@ -16,40 +16,48 @@
         <h3 class="panel-title">Machine Information</h3>
     </div>
     <div class="panel-body">
-        <p>
-        Machine ID: {{ $machine->id }} <br>
-        Manufacturer: {{ $machine->manufacturer->manufacturer }} <br>
-        Model: {{ $machine->model }} <br>
-        Serial Number: {{ $machine->serial_number }} <br>
-        Software version: {{ $machine->software_version }} <br>
-        Vendor Site ID: {{ $machine->vend_site_id }} <br>
-        Location: {{ $machine->location->location }} {{ $machine->room }}<br>
-        Manufacture Date: {{ $machine->manuf_date }} <br>
-        Install Date: {{ $machine->install_date }} <br>
-        Age: {{ $machine->age }}<br>
-        Status: {{ $machine->machine_status }}<br>
-        Notes: {{ $machine->notes }}
-        <div class="media">
-            <div class="media-right">
+        <div class="row">
+            <div class="col-md-4">
+            {{-- Machine data section --}}
+                <p>
+                Machine ID: {{ $machine->id }} <br>
+                Manufacturer: {{ $machine->manufacturer->manufacturer }} <br>
+                Model: {{ $machine->model }} <br>
+                Serial Number: {{ $machine->serial_number }} <br>
+                Software version: {{ $machine->software_version }} <br>
+                Vendor Site ID: {{ $machine->vend_site_id }} <br>
+                Location: {{ $machine->location->location }} {{ $machine->room }}<br>
+                Manufacture Date: {{ $machine->manuf_date }} <br>
+                Install Date: {{ $machine->install_date }} <br>
+                Age: {{ $machine->age }}<br>
+                Status: {{ $machine->machine_status }}<br>
+                Notes: {{ $machine->notes }}
+                <div class="media">
+                    <div class="media-right">
+                    </div>
+                </div>
+                </p>
+                @if (Auth::check())
+                <p>
+                    <form class="form-inline" action="{{ route('machines.destroy', $machine->id) }}" method="post">
+                        {{ csrf_field() }}
+                        {{ method_field('DELETE') }}
+                        <div class="form-group">
+                            <a href="{{ route('machines.edit', $machine->id) }}" class="btn btn-default btn-xs" role="button" data-toggle="tooltip" title="Modify this machine">
+                                <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+                            </a>
+                            <button type="submit" class="btn btn-danger btn-xs" data-toggle="tooltip" title="Remove this machine">
+                                <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+                            </button>
+                        </div>
+                    </form>
+                </p>
+                @endif
+            </div>
+            <div class="col-md-8">
+            {{-- Machine photo carousel --}}
             </div>
         </div>
-        </p>
-        @if (Auth::check())
-        <p>
-            <form class="form-inline" action="{{ route('machines.destroy', $machine->id) }}" method="post">
-                {{ csrf_field() }}
-                {{ method_field('DELETE') }}
-                <div class="form-group">
-                    <a href="{{ route('machines.edit', $machine->id) }}" class="btn btn-default btn-xs" role="button" data-toggle="tooltip" title="Modify this machine">
-                        <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-                    </a>
-                    <button type="submit" class="btn btn-danger btn-xs" data-toggle="tooltip" title="Remove this machine">
-                        <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
-                    </button>
-                </div>
-            </form>
-        </p>
-        @endif
     </div>
 </div>
 <h3><span class="label label-default">Tube Information</span></h3>
