d866ece69f2ddb8ec0b28c783d995dd50511abef
Eugene Mah <eugenemah@gmail.com>
2017-03-17 08:57:50 -0400

    Update jquery-migrate version

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -7,7 +7,7 @@
   src="https://code.jquery.com/jquery-3.1.1.min.js"
   integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
   crossorigin="anonymous"></script>
-<script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="https://code.jquery.com/jquery-migrate-3.0.0.min.js"></script>
 <script>window.jQuery || document.write('<script src="/js/vendor/jquery.min.js"><\/script>')</script>
 <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 {{-- Slick image carousel http://kenwheeler.github.io/slick/ --}}
