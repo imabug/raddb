84f79e814b8213a975bdffc8a7ecfc13fcf496d1
Eugene Mah <eugenemah@gmail.com>
2017-03-17 09:04:14 -0400

    Commenting

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -10,7 +10,8 @@
 <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-3.0.0.min.js"></script>
 <script>window.jQuery || document.write('<script src="/js/vendor/jquery.min.js"><\/script>')</script>
 <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-{{-- Slick image carousel http://kenwheeler.github.io/slick/ --}}
+{{-- Only load the Slick javascript if the machine detail page is being shown
+    Slick image carousel http://kenwheeler.github.io/slick/ --}}
 @if (Route::currentRouteName() == 'machines.show')
     <script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>
     <script type="text/javascript" src="{{ asset('js/slick_cfg.js') }}"></script>
