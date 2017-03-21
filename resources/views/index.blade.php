705ad1996cb9480a5ac58af52b581481f6d73ba5
Eugene Mah <eugenemah@gmail.com>
2017-03-21 08:26:06 -0400

    Put each section into tabs
    Yay Bootstrap.
    Fixes #147

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,20 @@
 @extends('layouts.app')
 
 @section('content')
-@include('inc.untested')
-@include('inc.pending')
-@include('inc.survey_schedule')
+<ul class="nav nav-pills" role="tablist">
+    <li role="presentation" class="active"><a href="#survey_schedule" aria-controls="survey_schedule" role="tab" data-toggle="pill">Survey Schedule</a></li>
+    <li role="presentation"><a href="#pending" aria-controls="pending" role="tab" data-toggle="pill">Pending</a></li>
+    <li role="presentation"><a href="#untested" aria-controls="untested" role="tab" data-toggle="pill">Untested</a></li>
+</ul>
+<div class="tab-content">
+    <div role="tabpanel" class="tab-pane active" id="survey_schedule">
+        @include('inc.survey_schedule')
+    </div>
+    <div role="tabpanel" class="tab-pane" id="pending">
+        @include('inc.pending')
+    </div>
+    <div role="tabpanel" class="tab-pane" id="untested">
+        @include('inc.untested')
+    </div>
+</div>
 @endsection
