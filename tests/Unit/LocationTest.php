d07050db19dee6bd3487e9beaa0af5db9ecad048
Eugene Mah <eugenemah@gmail.com>
2017-03-21 08:28:55 -0400

    Comment out unused items

--- tests/Unit/LocationTest.php
+++ tests/Unit/LocationTest.php
@@ -3,8 +3,8 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+// use Illuminate\Foundation\Testing\DatabaseMigrations;
+// use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class LocationTest extends TestCase
 {
