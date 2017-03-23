332d78566ae29427bbd26f3e3994ecffdd5d5420
Eugene Mah <eugenemah@gmail.com>
2017-03-23 15:37:16 -0400

    Try to write some tests for Location pages

--- tests/Unit/LocationTest.php
+++ tests/Unit/LocationTest.php
@@ -3,19 +3,30 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+use RadDB\Location;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 // use Illuminate\Foundation\Testing\DatabaseMigrations;
 // use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class LocationTest extends TestCase
 {
+    public function setup()
+    {
+        $this->location = new Location;
+    }
+
     /**
-     * A basic test example.
+     * Test to see if the location index page can be loaded
      *
      * @return void
      */
-    public function testExample()
+    public function testLocationIndexPage()
     {
-        $this->assertTrue(true);
+        $response = $this->get('/admin/locations');
+        $response->assertStatus(200);
+
+        $response = $this->visit('/admin/locations')
+             ->see('Locations');
     }
 }
