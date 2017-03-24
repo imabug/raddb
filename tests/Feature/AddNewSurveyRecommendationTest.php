614ab2f1db710b4e74d8638b349add0617eca73b
Eugene Mah <eugenemah@gmail.com>
2017-03-24 16:41:18 +0000

    Apply fixes from StyleCI

--- tests/Feature/AddNewSurveyRecommendationTest.php
+++ tests/Feature/AddNewSurveyRecommendationTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
+
 // use Illuminate\Foundation\Testing\DatabaseMigrations;
 // use Illuminate\Foundation\Testing\DatabaseTransactions;
 
