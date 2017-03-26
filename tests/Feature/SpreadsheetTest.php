309bf1b0d16e7be4a718ac3a4124027b22213bda
Eugene Mah <eugenemah@gmail.com>
2017-03-26 10:50:49 -0400

    Test spreadsheet processing with phpoffice

---
+++ tests/Feature/SpreadsheetTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class SpreadsheetTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testLoadingSpreadsheetFile()
+    {
+        $page = $this->get(route('test.loadSpreadsheet'));
+        $page->assertStatus(200);
+        
+    }
+}
