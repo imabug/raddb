ad274589b81da4afd2b1a49651499ac00974edb1
Eugene Mah <eugenemah@gmail.com>
2017-03-30 14:41:59 -0400

    Start working on fluoro spreadsheet import

--- app/Console/Commands/ImportFluoroSpreadsheet.php
+++ app/Console/Commands/ImportFluoroSpreadsheet.php
@@ -2,6 +2,11 @@
 
 namespace RadDB\Console\Commands;
 
+use PHPExcel;
+use RadDB\Tube;
+use RadDB\HVLData;
+use RadDB\Machine;
+use RadDB\TestDate;
 use Illuminate\Console\Command;
 
 class ImportFluoroSpreadsheet extends Command
@@ -11,7 +16,7 @@ class ImportFluoroSpreadsheet extends Command
      *
      * @var string
      */
-    protected $signature = 'import:fluoro';
+    protected $signature = 'import:fluoro {file}';
 
     /**
      * The console command description.
@@ -40,12 +45,49 @@ class ImportFluoroSpreadsheet extends Command
         $options = $this->option();
         $spreadsheetFile = $this->argument('file');
 
+        // Read the spreadsheet
+        echo "Loading spreadsheet\n";
         $reader = \PHPExcel_IOFactory::createReader('OOCalc');
         $reader->setReadDataOnly(true);
-        $spreadsheet = $reader->load($file);
-        $genFormSheet = $spreadsheet->getSheetByName('Fluoro');
+        $spreadsheet = $reader->load($spreadsheetFile);
+        $fluoroSheet = $spreadsheet->getSheetByName('Fluoro');
+
+        // Get the survey ID
         $surveyId = (int) $fluoroSheet->getCell('F13')->getCalculatedValue();
+        echo 'Saving data for survey ID: '.$surveyId."\n";
+
+        // Pull info for this spreadsheet from the database
+        $survey = TestDate::find(1939);
+        $machine = Machine::find($survey->machine_id);
+        $tubes = Tube::where('machine_id', $machine->id)->active()->get();
+
+        // There might be more than one tube associated with this machine, so
+        // ask the user which tube to associate this spreadsheet with
+        if ($tubes->count() > 1) {
+            $choice = "Enter the tube ID this spreadsheet belongs to\n";
+            $tubeChoice = [];
+            foreach ($tubes as $t) {
+                $choice .= $t->id.': '.$t->insert_sn."\n";
+                $tubeChoice[] = $t->id;
+            }
+            $tubeId = $this->choice($choice, $tubeChoice);
+        } else {
+            $tubeId = $tubes->first()->id;
+        }
+
+        // Get HVL
         $hvl = (float) $fluoroSheet->getCell('X174')->getCalculatedValue();
+        $hvl_kv = (float) $fluoroSheet->getCell('F137')->getCalculatedValue();
+        // Store HVL to database
+        $HVLData = new HVLData();
+        $HVLData->survey_id = $survey->id;
+        $HVLData->machine_id = $machine->id;
+        $HVLData->tube_id = $tubeId;
+        $HVLData->kv = (float) $hvl_kv;
+        $HVLData->hvl = (float) $hvl;
+        $HVLData->save();
+        echo "HVL data saved.\n";
+
         $fluoroEntranceExpRate = $fluoroSheet->rangeToArray('C205:M219', null, true, false, false);
         $maxFluoroEntraceExpRate = $fluoroSheet->rangeToArray('E220:M220', null, true, false, false);
         $fluoroReceptorEntrExpRate = $fluoroSheet->rangeToArray('C237:H251', null, true, false, false);
