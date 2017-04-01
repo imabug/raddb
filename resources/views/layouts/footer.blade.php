98426dffbdeb5b7ae0d2c026f780939886eaa635
Eugene Mah <eugenemah@gmail.com>
2017-04-01 08:04:43 -0400

    Update jQuery
    Update jQuery to 3.2.1.
    Update jQuery-migrate
    Add jQuery-UI 1.12.1

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,11 +4,18 @@
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
 <script
-  src="https://code.jquery.com/jquery-3.1.1.min.js"
-  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-  crossorigin="anonymous"></script>
-<script type="text/javascript" src="https://code.jquery.com/jquery-migrate-3.0.0.min.js"></script>
+    src="https://code.jquery.com/jquery-3.2.1.min.js"
+    integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+    crossorigin="anonymous"></script>
+<script
+    src="https://code.jquery.com/jquery-migrate-3.0.0.min.js"
+    integrity="sha256-JklDYODbg0X+8sPiKkcFURb5z7RvlNMIaE3RA2z97vw="
+    crossorigin="anonymous"></script>
 <script>window.jQuery || document.write('<script src="/js/vendor/jquery.min.js"><\/script>')</script>
+<script
+    src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
+    integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
+    crossorigin="anonymous"></script>
 <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 {{-- Only load the Slick javascript if the machine detail page is being shown
     Slick image carousel http://kenwheeler.github.io/slick/ --}}
