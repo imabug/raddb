e8902ea57bc01fd89b8ba5483b2e0207491bf7c7
Eugene Mah <eugenemah@gmail.com>
2017-05-10 08:48:33 -0400

    Forgot to include Schema facade

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace RadDB\Providers;
 
 use Laravel\Dusk\DuskServiceProvider;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
