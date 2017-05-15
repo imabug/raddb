8707883a5eda97de904b70945833771f8c7852f4
Eugene Mah <eugenemah@gmail.com>
2017-05-15 12:45:55 -0400

    Add laravel-artisan-dd package from https://github.com/spatie/laravel-artisan-dd/

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         Commands\ImportRadSpreadsheet::class,
         Commands\ImportFluoroSpreadsheet::class,
+        \Spatie\ArtisanDd\DdCommand::class,
     ];
 
     /**
