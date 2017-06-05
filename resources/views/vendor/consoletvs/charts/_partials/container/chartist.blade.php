9a41b0e989afb4ce55bf57d6466af18b1d41410d
Eugene Mah <eugenemah@gmail.com>
2017-06-05 17:46:28 -0400

    republish vendor resources

--- resources/views/vendor/consoletvs/charts/_partials/container/chartist.blade.php
+++
@@ -1,5 +0,0 @@
-@if(!$model->container)
-	@include('charts::_partials.container.title')
-
-	<div id="{{ $model->id }}" style="@include('charts::_partials.dimension.css')" class="ct-chart ct-perfect-fourth"></div>
-@endif
--- resources/views/vendor/consoletvs/charts/_partials/container/svg.blade.php
+++
@@ -1,3 +0,0 @@
-@if(!$model->container)
-	<svg id="{{ $model->id }}" @include('charts::_partials.dimension.html')></svg>
-@endif
