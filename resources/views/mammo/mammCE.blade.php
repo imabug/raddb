a5a1643920848d4a21967de7d0284960d8c0d5d5
Eugene Mah <eugenemah@gmail.com>
2017-08-08 12:29:37 -0400

    Make the output look prettier

--- resources/views/mammo/mammCE.blade.php
+++ resources/views/mammo/mammCE.blade.php
@@ -7,17 +7,37 @@
 <p>Report generated {{ $reportDate }}</p>
 <h3>Mammography Units</h3>
 @foreach ($mammoMachines as $mamm)
-    <h4>{{ $mamm->description }}</h4>
-    @foreach ($mamm->testdate->take(-2) as $testdate)
-        <p>{{ $testdate->test_date }}</p>
-    @endforeach
+<table class="table">
+    <thead>
+        <tr>
+            <th colspan="2">{{ $mamm->description }}</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+        @foreach ($mamm->testdate->take(-2) as $testdate)
+            <td>{{ $testdate->test_date }}</td>
+        @endforeach
+        </tr>
+    </tbody>
+</table>
 @endforeach
 
 <h3>Mammography Workstations</h3>
 @foreach ($mammoWorkstations as $wrk)
-    <h4>{{ $wrk->description }}</h4>
-    @foreach ($wrk->testdate->take(-2) as $wrkDate)
-        <p>{{ $wrkDate->test_date }}</p>
-    @endforeach
+<table class="table">
+    <thead>
+        <tr>
+            <th colspan="2">{{ $wrk->description }}</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+        @foreach ($wrk->testdate->take(-2) as $wrkDate)
+            <td>{{ $wrkDate->test_date }}</td>
+        @endforeach
+        </tr>
+    </tbody>
+</table>
 @endforeach
 @endsection
