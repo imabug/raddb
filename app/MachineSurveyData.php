1d9c137401d7be71e6493fa52dee6ed14dd0e795
Eugene Mah <eugenemah@gmail.com>
2017-08-25 14:58:30 -0400

    Migration and model for machine/survey data table

---
+++ app/MachineSurveyData.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace RadDB;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MachineSurveyData extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'machine_surveydata';
+
+    /**
+     * Attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'machine_id',
+        'survey_id',
+        'gendata',
+        'collimatordata',
+        'radoutputdata',
+        'radsurveydata',
+        'hvldata',
+        'fluorodata',
+        'maxfluorodata',
+        'receptorentrance',
+    ];
+
+    /**
+     * Attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'deleted_at',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Relationships
+     */
+    public function machine()
+    {
+        return $this->belongsTo('RadDB\Machine');
+    }
+
+    public function survey()
+    {
+        return $this->belongsTo('RadDB\TestDate');
+    }
+}
---
+++ database/migrations/2017_08_25_123018_add_machine_surveydata_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddMachineSurveydataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('machine_surveydata', function (Blueprint $table) {
+            $table->integer('machine_id')->unsigned();
+            $table->integer('survey_id')->unsigned();
+            $table->boolean('gendata')->nullable();
+            $table->boolean('collimatordata')->nullable();
+            $table->boolean('radoutputdata')->nullable();
+            $table->boolean('radsurveydata')->nullable();
+            $table->boolean('hvldata')->nullable();
+            $table->boolean('fluorodata')->nullable();
+            $table->boolean('maxfluorodata')->nullable();
+            $table->boolean('receptorentrance')->nullable();
+            $table->index(['machine_id', 'survey_id']);
+            $table->foreign('machine_id')->references('id')->on('machines');
+            $table->foreign('survey_id')->references('id')->on('testdates');
+            $table->softDeletes();
+            $table->timestamps();
+        })
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
