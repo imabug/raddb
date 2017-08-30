9e5927af515d7ef41262425659f6f450a02f7cac
Eugene Mah <eugenemah@gmail.com>
2017-08-30 16:42:25 -0400

    Remove laravelcollective aliases

--- config/app.php
+++ config/app.php
@@ -228,8 +228,6 @@ return [
         'Validator'    => Illuminate\Support\Facades\Validator::class,
         'View'         => Illuminate\Support\Facades\View::class,
         'Charts'       => ConsoleTVs\Charts\Facades\Charts::class,
-        'Form'         => Collective\Html\FormFacade::class,
-        'Html'         => Collective\Html\HtmlFacade::class,
     ],
 
 ];
