80124e822b949a0c579b932a511131367b841c86
Eugene Mah <eugenemah@gmail.com>
2017-08-31 13:02:31 -0400

    Add @dump Blade directive

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace RadDB\Providers;
 
 use Laravel\Dusk\DuskServiceProvider;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -17,6 +18,20 @@ class AppServiceProvider extends ServiceProvider
     {
         // Work around for MariaDB/MySQL key too long errors
         Schema::defaultStringLength(191);
+
+        /**
+         * Blade directive to dump a variable/object inside a template.
+         * This is similar to dd(), except that it doesn't interrupt the
+         * execution of the app. It does NOT support multiple arguments
+         * however, you have to use one directive per variable.
+         *
+         * From https://gist.github.com/victorloux/0c073afa5d4784d2b8e9
+         *
+         * @example @dump($posts->comments)
+         */
+        Blade::directive('dump', function($param) {
+            return "<pre><?php (new \Illuminate\Support\Debug\Dumper)->dump($param); ?></pre>";
+        });
     }
 
     /**
