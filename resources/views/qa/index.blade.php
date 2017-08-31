3c19eeee25cff9ee30a15b3fdba56198f0747938
Eugene Mah <eugenemah@gmail.com>
2017-08-31 13:03:32 -0400

    View to show list of active machines with survey data

--- resources/views/qa/index.blade.php
+++ resources/views/qa/index.blade.php
@@ -1,6 +1,16 @@
 <!-- resources/views/qa/index.blade.php -->
 
 @extends('qa.app')
-
 @section('content')
+<h2><span class="label label-default">Active machines with survey data</span></h2>
+
+<table class="table table-hover">
+@foreach ($machines->chunk(5) as $chunk)
+<tr>
+@foreach ($chunk as $m)
+<td><a href="{{ route('qa.show', $m->id) }}">{{ $m->description }}</a></td>
+@endforeach
+</tr>
+@endforeach
+</table>
 @endsection
