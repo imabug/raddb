44e298cb0b8ed5dbae1d5438d3a95558fe34c990
Eugene Mah <eugenemah@gmail.com>
2017-09-01 12:48:28 -0400

    Basic page to show radiation output

--- app/Http/Controllers/RadiationOutputController.php
+++ app/Http/Controllers/RadiationOutputController.php
@@ -2,6 +2,8 @@
 
 namespace RadDB\Http\Controllers;
 
+use Charts;
+use RadDB\TestDate;
 use RadDB\RadiationOutput;
 use Illuminate\Http\Request;
 
@@ -41,12 +43,30 @@ class RadiationOutputController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \RadDB\RadiationOutput  $radiationOutput
+     * @param int $surveyId
      * @return \Illuminate\Http\Response
      */
-    public function show(RadiationOutput $radiationOutput)
+    public function show(int $surveyId)
     {
-        //
+        $survey = TestDate::with('machine')->findOrFail($surveyId);
+        $radOutput = RadiationOutput::where('survey_id', $surveyId)->orderBy('kv')->get();
+        $radOutputChart = Charts::create('scatter', 'google')
+                        ->labels($radOutput->pluck('kv'))
+                        ->values($radOutput->pluck('output'))
+                        ->elementLabel('Output')
+                        ->title('Radiation Output')
+                        ->xAxisTitle('kV')
+                        ->yAxisTitle('mGy/mAs @ 40 in.')
+                        ->responsive(false)
+                        ->height(600)
+                        ->width(800)
+                        ->legend(true);
+
+        return view('qa.radoutput.show', [
+            'survey' => $survey,
+            'radOutput' => $radOutput,
+            'radOutputChart' => $radOutputChart,
+        ]);
     }
 
     /**
