9906a9f2209193ddb73895f96d8c3dc422aebcee
Eugene Mah <eugenemah@gmail.com>
2017-10-25 13:27:35 -0400

    New migration to add target/filter field
    Add target_filter field to hvldata, radoutputdata, radsurveydata tables
    to accomodate storing test data from mammo units

---
+++ database/migrations/2017_10_25_131745_add_target_filter_to_data_tables.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTargetFilterToDataTables extends Migration
+{
+    /**
+     * Add target_filter field (i.e. Mo/Mo, W/Rh) to accomodate mammography
+     * data.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Add a string field to hold mammo target/filter combination
+        // Field will contain data in the format "target/filter"
+        // i.e. Mo/Mo, Mo/Rh, W/Rh, etc.
+        Schema::table('hvldata', function (Blueprint $table) {
+            $table->string('target_filter', 6)->nullable()->after('tube_id');
+        });
+        Schema::table('radoutput', function (Blueprint $table) {
+            $table->string('target_filter', 6)->nullable()->after('tube_id');
+        });
+        Schema::table('radsurveydata', function (Blueprint $table) {
+            $table->string('target_filter', 6)->nullable()->after('tube_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('hvldata', function (Blueprint $table) {
+            $table->dropColumn('target_filter');
+        });
+        Schema::table('radoutput', function (Blueprint $table) {
+            $table->dropColumn('target_filter');
+        });
+        Schema::table('radsurveydata', function (Blueprint $table) {
+            $table->dropColumn('target_filter');
+        });
+    }
+}
