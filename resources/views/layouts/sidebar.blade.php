77b7df8607234831c2eade962fcffa22bbd34735
Eugene Mah <eugenemah@gmail.com>
2017-11-15 10:18:20 -0500

    Add nav bar toggle

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,5 +1,14 @@
 <nav class="navbar navbar-default navbar-static-top">
-    <div class="container-fluid">
+    <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">RadDB</a>
+    </div>
     <div id="navbar" class="navbar-collapse collapse">
         <ul class="nav navbar-nav nav-tabs">
             <li role="presentation" class="active"><a href="{{ route('index') }}">Home</a></li>
