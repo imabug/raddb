9d8173417fb89de18f592d62d0346d950025404d
Eugene Mah <eugenemah@gmail.com>
2017-11-15 14:48:21 -0500

    Have brand tag link to index page

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -7,7 +7,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="#">RadDB</a>
+        <a class="navbar-brand" href="{{ route('index') }}">RadDB</a>
     </div><!--/.nav-header -->
     <div id="navbar" class="navbar-collapse collapse">
         <ul class="nav navbar-nav nav-tabs">
