866bf3b1abdfe6fa1cf8e66d41cd406f9e274bda
Eugene Mah <eugenemah@gmail.com>
2017-11-20 15:07:29 -0500

    Model files for Leeds test objects data

---
+++ app/FluoroResolution.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace RadDB;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FluoroResolution extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'fluoro_resolution';
+
+    /**
+     * Attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'field_size',
+        'resolution',
+    ];
+
+    /**
+     * Attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'deleted_at',
+        'created_at',
+        'updated_at',
+    ];
+
+    /*
+     * Relationships
+     */
+     public function machine()
+     {
+         return $this->belongsTo('RadDB\Machine');
+     }
+
+     public function survey()
+     {
+         return $this->belongsTo('RadDB\TestDate');
+     }
+
+     public function tube()
+     {
+         return $this->belongsTo('RadDB\Tube');
+     }
+}
