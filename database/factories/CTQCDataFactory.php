26948a17745e7c514c8eb86bbea6ba2d396344a9
Eugene Mah <eugenemah@gmail.com>
2018-05-11 12:59:55 -0400

    Documentation for CT QC data factory

--- database/factories/CTQCDataFactory.php
+++ database/factories/CTQCDataFactory.php
@@ -2,14 +2,17 @@
 
 use Faker\Generator as Faker;
 
-use Carbon\Carbon;
+/**
+ * Model factory to generate random CT QC data.
+ * A random date within the most recent three months is used for the QC date.
+ */
 
 $factory->define(RadDB\CTDailyQCRecord::class, function (Faker $faker) {
     return [
         'machine_id' => '130',
-        'qcdate' => $faker->dateTimeThisMonth()->format('Y-m-d'),
+        'qcdate' => $faker->dateTimeBetween('-3 months', 'now')->format('Y-m-d'),
         'scan_type' => $faker->randomElement(['Axial', 'Helical']),
-        'water_hu' => $faker->randomFloat(1, -10, 10),
+        'water_hu' => $faker->randomFloat(1, -15, 15),
         'water_sd' => $faker->randomFloat(1, 0, 10),
         'artifacts' => $faker->randomElement(['Y', 'N']),
         'initials' => 'EM',
