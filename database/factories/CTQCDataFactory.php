3d602fa7f4b688e0e2e7e53655d11c6c98cda0d7
Eugene Mah <eugenemah@gmail.com>
2018-05-11 10:22:21 -0400

    Add model factory to generate random CT QC data

---
+++ database/factories/CTQCDataFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+use Faker\Generator as Faker;
+
+use Carbon\Carbon;
+
+$factory->define(RadDB\CTDailyQCRecord::class, function (Faker $faker) {
+    return [
+        'machine_id' => '130',
+        'qcdate' => $faker->dateTimeThisMonth()->format('Y-m-d'),
+        'scan_type' => $faker->randomElement(['Axial', 'Helical']),
+        'water_hu' => $faker->randomFloat(1, -10, 10),
+        'water_sd' => $faker->randomFloat(1, 0, 10),
+        'artifacts' => $faker->randomElement(['Y', 'N']),
+        'initials' => 'EM',
+        'notes' => $faker->sentence(),
+    ];
+});
