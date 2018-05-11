a56758dbe49a37329c3653d19a3828c932866792
Eugene Mah <eugenemah@gmail.com>
2018-05-11 10:22:42 -0400

    DB seeder for CT QC data

---
+++ database/seeds/CTQCDataSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CTQCDataSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(RadDB\CTDailyQCRecord::class,100)->create();
+    }
+}
