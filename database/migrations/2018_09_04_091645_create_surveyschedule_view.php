2c30a9b9e8302aa5e909db638b9f925d0811000c
Eugene Mah <eugenemah@gmail.com>
2018-09-04 10:42:27 -0400

    Remove indentation from SQL

--- database/migrations/2018_09_04_091645_create_surveyschedule_view.php
+++ database/migrations/2018_09_04_091645_create_surveyschedule_view.php
@@ -17,14 +17,14 @@ class CreateSurveyscheduleView extends Migration
         return <<<SQL
 create view surveyschedule_view as 
 select machines.id,machines.description,
-    lastyear_view.survey_id as prevSurveyID ,
-    lastyear_view.test_date as prevSurveyDate,
-    lastyear_view.report_file_path as prevSurveyReport,
-    lastyear_view.recCount as prevRecCount,
-    thisyear_view.survey_id as currSurveyID,
-    thisyear_view.test_date as currSurveyDate,
-    thisyear_view.report_file_path as currSurveyReport,
-    thisyear_view.recCount as currRecCount
+lastyear_view.survey_id as prevSurveyID ,
+lastyear_view.test_date as prevSurveyDate,
+lastyear_view.report_file_path as prevSurveyReport,
+lastyear_view.recCount as prevRecCount,
+thisyear_view.survey_id as currSurveyID,
+thisyear_view.test_date as currSurveyDate,
+thisyear_view.report_file_path as currSurveyReport,
+thisyear_view.recCount as currRecCount
 from machines
 left join thisyear_view on machines.id = thisyear_view.machine_id
 left join lastyear_view on machines.id = lastyear_view.machine_id
