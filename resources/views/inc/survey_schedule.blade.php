cd9e9da4989e0d953e2f4c7f58d541c11e92d310
Eugene Mah <eugenemah@gmail.com>
2018-09-11 14:44:42 -0400

    Show survey report status
    Show a document icon when a survey report is available via spatie/medialibrary

--- resources/views/inc/survey_schedule.blade.php
+++ resources/views/inc/survey_schedule.blade.php
@@ -16,19 +16,33 @@
 		<tr>
 			<td>{{ $ss->id }}</td>
 			<td><a href="{{ route('machines.show', $ss->id) }}">{{ $ss->description }}</a></td>
-        @if ($ss->prevRecCount > 0)
+@if ($ss->prevRecCount > 0)
             <td><a href="{{ route('recommendations.show', $ss->prevSurveyID)}}" title="Recommendations" alt="Recommendations">{{ $ss->prevSurveyDate }}</a></td>
-        @else
+@else
             <td>{{ $ss->prevSurveyDate }}</td>
-        @endif
-            <td>{{ $ss->prevSurveyID }}</td>
-        @if ($ss->currRecCount > 0)
+@endif
+    <td>{{ $ss->prevSurveyID }}
+@if (is_null($ss->prevSurvey))
+    </td>
+@else
+    @if($ss->prevSurvey->hasMedia('survey_report'))
+        <a href="{{ $ss->prevSurvey->getFirstMediaUrl('survey_report') }}" target="_blank" title="Survey report" alt="Survey report"><span class="glyphicon glyphicon-file" aria-hidden="true"></span></a></td>
+    @endif
+@endif
+@if ($ss->currRecCount > 0)
             <td><a href="{{ route('recommendations.show', $ss->currSurveyID)}}" title="Recommendations" alt="Recommendations">{{ $ss->currSurveyDate }}</a></td>
-        @else
+@else
             <td>{{ $ss->currSurveyDate }}</td>
-        @endif
-            <td>{{ $ss->currSurveyID }}</td>
-		</tr>
+@endif
+    <td>{{ $ss->currSurveyID }}
+@if (is_null($ss->currSurvey))
+    </td>
+@else
+    @if($ss->currSurvey->hasMedia('survey_report'))
+        <a href="{{ $ss->currSurvey->getFirstMediaUrl('survey_report') }}" target="_blank" title="Survey report" alt="Survey report"><span class="glyphicon glyphicon-file" aria-hidden="true"></span></a></td>
+    @endif
+@endif
+</tr>
 @endforeach
 	</tbody>
 </table>
