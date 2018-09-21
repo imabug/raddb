b8e10b4487d2fc1e9920af53922ee401da3bb2c1
Eugene Mah <eugenemah@gmail.com>
2018-09-21 09:37:07 -0400

    Update Users to match laravel/laravel repo

--- app/User.php
+++ app/User.php
@@ -3,6 +3,7 @@
 namespace RadDB;
 
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
