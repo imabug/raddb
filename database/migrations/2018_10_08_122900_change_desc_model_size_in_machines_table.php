6597308a24c3e1b08afe7b9cd9bfcd53057d68e3
Eugene Mah <eugenemah@gmail.com>
2018-10-08 12:34:43 -0400

    Change size of description, model in machines table

---
+++ database/migrations/2018_10_08_122900_change_desc_model_size_in_machines_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeDescModelSizeInMachinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('machines', function (Blueprint $table) {
+            $table->string('model', 100)->change();
+            $table->string('description', 100)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('machines', function (Blueprint $table) {
+            $table->string('model', 50)->change();
+            $table->string('description', 60)->change();
+        });
+    }
+}
