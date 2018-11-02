0427944288beefb6f871a354245f2eeb8ac33d81
Eugene Mah <eugenemah@gmail.com>
2018-11-02 14:35:17 -0400

    New artisan command to remove an entry from a lookup table

---
+++ app/Console/Commands/LutDelete.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace RadDB\Console\Commands;
+
+use RadDB\Tester;
+use RadDB\Location;
+use RadDB\Modality;
+use RadDB\TestType;
+use RadDB\Manufacturer;
+use Illuminate\Console\Command;
+
+class LutDelete extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'lut:delete
+                            {table : The lookup table to delete from}
+                            {value : Value to remove from the lookup table}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Remove an entry from a lookup table.  Lookup table is one of location, manufacturer, modality, tester, or testtype';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $table = strtolower($this->argument('table'));
+        $tableValue = $this->argument('value');
+        $headers = ['ID', $table];
+
+        // Show the lookup table
+        $this->call('lut:list', [
+            'table' => $table,
+        ]);
+
+        switch($table) {
+        case 'location':
+            $lut = Location::where('location', $tableValue)->first();
+            break;
+        case 'manufacturer':
+            $lut = Manufacturer::where('manufacturer', $tableValue)->first();
+            break;
+        case 'modality':
+            $lut = Modality::where('modality', $tableValue)->first();
+            break;
+        case 'tester':
+            $lut = Tester::where('tester', $tableValue)->first();
+            break;
+        case 'testtype':
+            $lut = TestType::where('test_type', $tableValue)->first();
+            break;
+        default:
+            break;
+        }
+
+        // Ask for confirmation
+        if($this->confirm('Deleting '.$table.' ID:'.$lut->id.'. Do you wish to continue?')) {
+            $lut->delete();
+        }
+
+        // Show the lookup table
+        $this->call('lut:list', [
+            'table' => $table,
+        ]);
+
+        $this->info($table.' ID:'.$lut->id.' deleted.');
+
+        return true;
+    }
+}
