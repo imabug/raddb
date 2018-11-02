e4c013f9f444cfda6fa9f40b77df9c20b578a3bf
Eugene Mah <eugenemah@gmail.com>
2018-11-02 13:21:16 -0400

    New artisan command to add a new entry to lookup tables

---
+++ app/Console/Commands/LutAdd.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace RadDB\Console\Commands;
+
+use RadDB\Tester;
+use RadDB\Location;
+use RadDB\Modality;
+use RadDB\TestType;
+use RadDB\Manufacturer;
+use Illuminate\Console\Command;
+
+class LutAdd extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'lut:add
+                            {table : The lookup table to add to}
+                            {value : Value to add to the lookup table}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Add a new entry to one of the lookup tables.  Lookup table is one of location, manufacturer, modality, tester, or testtype';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $table = strtolower($this->argument('table'));
+        $tableValue = $this->argument('value');
+        $headers = ['ID', $table];
+
+        switch($table) {
+        case 'location':
+            $location = Location::create(['location' => $tableValue]);
+            $body = Location::all(['id', 'location'])->toArray();
+            break;
+        case 'manufacturer':
+            $manufacturer = Manufacturer::create(['manufacturer' => $tableValue]);
+            $body = Manufacturer::all(['id', 'manufacturer'])->toArray();
+            break;
+        case 'modality':
+            $modality = Modality::create(['modality' => $tableValue]);
+            $body = Modality::all(['id', 'modality'])->toArray();
+            break;
+        case 'tester':
+            $tester = Tester::create(['tester' => $tableValue]);
+            $body = Tester::all(['id', 'name'])->toArray();
+            break;
+        case 'testtype':
+            $testtype = TestType::create(['test_type' => $tableValue]);
+            $body = TestType::all(['id', 'test_type'])->toArray();
+            break;
+        default:
+            break;
+        }
+        $this->table($headers, $body);
+
+        return true;
+    }
+}
