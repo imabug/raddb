b2c7e623f1fda96588afbbc9a33746e945b36f67
Eugene Mah <eugenemah@gmail.com>
2018-11-07 14:53:00 -0500

    Artisan command to add a new machine.
    Not finished yet. Still needs testing.

--- app/Console/Commands/MachineAdd.php
+++ app/Console/Commands/MachineAdd.php
@@ -2,6 +2,10 @@
 
 namespace RadDB\Console\Commands;
 
+use RadDB\Machine;
+use RadDB\Location;
+use RadDB\Modality;
+use RadDB\Manufacturer;
 use Illuminate\Console\Command;
 
 class MachineAdd extends Command
@@ -11,14 +15,14 @@ class MachineAdd extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'machine:add';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Add a new machine to the database';
 
     /**
      * Create a new command instance.
@@ -37,6 +41,35 @@ class MachineAdd extends Command
      */
     public function handle()
     {
-        //
+        $locations = Location::all(['id', 'location']);
+        $modalities = Modality::all(['id', 'modality']);
+        $manufacturers = Manufacturer::all(['id', 'manufacturer']);
+        $locHeader = ['ID', 'Location'];
+        $modHeader = ['ID', 'Modality'];
+        $manufHeader = ['ID', 'Manufacturer'];
+
+        $machine = new Machine;
+
+        $machine->description = $this->ask('Give a descriptive name for this machine');
+
+        $this->table($locHeader, $locations->toArray());
+        $machine->location_id = $this->ask('Enter the location ID for this machine');
+
+        $this->table($modHeader, $modalities->toArray());
+        $machine->modality_id = $this->ask('Enter the modality ID for this machine');
+
+        $this->table($manufHeader, $manufacturers->toArray());
+        $machine->manufacturer_id = $this->ask('Enter the manufacturer ID for this machine');
+
+        $machine->model = $this->ask('Enter the model name for this machine');
+        $machine->serial_number = $this->ask('Enter the serial number for this machine');
+        $machine->manuf_date = $this->ask('Enter the manufacture date for this machine (YYYY-MM-DD)');
+        $machine->install_date = $this->ask('Enter the installation date for this machine (YYYY-MM-DD)');
+        $machine->room = $this->ask('Enter the room number for this machine');
+        $machine->vend_site_id = $this->ask('Enter the vendor site ID for this machine');
+        $machine->notes = $this->ask('Enter any special notes for this machine');
+        $machine->machine_status = 'Active';
+
+        dump($machine);
     }
 }
