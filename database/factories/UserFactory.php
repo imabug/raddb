077ad3d6d6055255bca4cf14b2addfabbea9fa80
Eugene Mah <eugenemah@gmail.com>
2019-05-30 11:09:54 -0400

    Fix conflicts
    https://github.com/laravel/laravel/commit/4b78ba61eac32cefa41da09149e0500e23cb06ec

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,5 +1,8 @@
 <?php
 
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+use App\User;
+use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
 /*
@@ -12,12 +15,12 @@ use Faker\Generator as Faker;
 | model instances for testing / seeding your application's database.
 |
 */
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'remember_token' => Str::random(10),
     ];
 });
