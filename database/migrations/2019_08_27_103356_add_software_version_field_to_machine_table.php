ffe09fdf824ecf5f645a8b66ff01bd903585cbdf
Eugene Mah <eugenemah@gmail.com>
2019-08-27 11:02:52 -0400

    Migration to add software version to machine table

---
+++ database/migrations/2019_08_27_103356_add_software_version_field_to_machine_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSoftwareVersionFieldToMachineTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('machine', function (Blueprint $table) {
+            $table->string('software_version', 50)
+                ->nullable()
+                ->comment('Machine software version');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('machine', function (Blueprint $table) {
+            $table->dropColumn('software_version');
+        });
+    }
+}
