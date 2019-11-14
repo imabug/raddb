6923e4234383c786cb17dcccfce5b34e7e9b1372
Eugene Mah <eugenemah@gmail.com>
2019-11-14 12:11:43 -0500

    password confirmation

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -27,13 +27,4 @@ class ResetPasswordController extends Controller
      */
     protected $redirectTo = '/';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest');
-    }
 }
