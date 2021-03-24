4ab3ae399f48e92ee62a749346ebd9b4bdb3f533
Eugene Mah <eugenemah@gmail.com>
2021-03-24 13:53:22 -0400

    Upgrade to spatie/medialibrary v9.x

--- app/Recommendation.php
+++ app/Recommendation.php
@@ -4,14 +4,14 @@ namespace RadDB;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Spatie\MediaLibrary\HasMedia\HasMedia;
-use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\Models\Media;
 
 class Recommendation extends Model
 {
     use SoftDeletes;
-    use HasMediaTrait;
+    use InteractsWithMedia;
 
     /**
      * Attributes that are mass assignable.
--- app/TestDate.php
+++ app/TestDate.php
@@ -4,14 +4,14 @@ namespace RadDB;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Spatie\MediaLibrary\HasMedia\HasMedia;
-use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\Models\Media;
 
 class TestDate extends Model implements HasMedia
 {
     use SoftDeletes;
-    use HasMediaTrait;
+    use InteractsWithMedia;
 
     /**
      * The table associated with the model.
