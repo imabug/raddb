5617828a0db1ae62bc43535e8a57dc69e7e7e55b
Eugene Mah <eugenemah@gmail.com>
2021-05-10 11:44:41 -0400

    New unit tests for Dashboard controller

---
+++ tests/Unit/DashboardControllerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use RadDB\Dashboard;
+use RadDB\Machine;
+use RadDB\SurveyScheduleView;
+use RadDB\TestDate;
+
+class DashboardControllerTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
