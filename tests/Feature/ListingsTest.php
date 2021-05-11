7f8f311f6e8d466878cdc6635fbe48b3319b77d5
Eugene Mah <eugenemah@gmail.com>
2021-05-11 10:31:42 -0400

    Test http requests for listing pages

--- tests/Feature/ListingsTest.php
+++ tests/Feature/ListingsTest.php
@@ -11,6 +11,7 @@ class ListingsTest extends TestCase
 
     /**
      * Test to see that the Listings pages load.
+     *
      * @dataProvider listingRoutes
      * @return void
      */
@@ -30,6 +31,31 @@ class ListingsTest extends TestCase
             'Removed machines listing' => ['machines.removed', 'Equipment Inventory - Removed'],
             'Test equipment listing' => ['testequipment.index', 'Equipment Inventory - Test Equipment'],
             'Test equipment cal dates listing' => ['testequipment.showCalDates', 'Recent Test Equipment Calibration Dates'],
+        ];
+    }
+
+    /**
+     * Test that the Listings pages load via HTTP requests
+     *
+     * @dataProvider ListingPages
+     * @return void
+     **/
+    public function testListingPagesLoadViaHttp($pageName, $value)
+    {
+        $this->get($pageName)->assertStatus(200);
+    }
+
+    public function listingPages(): array
+    {
+        return [
+            'Machines listing' => ['/machines', 'Equipment Inventory - Active'],
+            'Modality listing' => ['/machines/modalities', 'List equipment by modality'],
+            'Location listing' => ['/machines/locations', 'List equipment by location'],
+            'Manufacturer listing' => ['/machines/manufacturers', 'List equipment by manufacturer'],
+            'Inactive machines listing' => ['/machines/inactive', 'Equipment Inventory - Inactive'],
+            'Removed machines listing' => ['/machines/removed', 'Equipment Inventory - Removed'],
+            'Test equipment listing' => ['/testequipment', 'Equipment Inventory - Test Equipment'],
+            'Test equipment cal dates listing' => ['/testequipment/caldates', 'Recent Test Equipment Calibration Dates'],
         ];
     }
 }
