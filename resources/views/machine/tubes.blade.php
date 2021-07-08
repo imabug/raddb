72308da0a2e434f050b7f316a643b176fa1c985f
Eugene Mah <eugenemah@gmail.com>
2021-07-08 15:46:18 -0400

    Fix typos and replace more glyphicons

--- resources/views/machine/tubes.blade.php
+++ resources/views/machine/tubes.blade.php
@@ -37,7 +37,7 @@
 					{{ method_field('DELETE') }}
 					<div class="col-auto">
                         <a href="{{ route('machines.edit', $machine->id) }}" data-toggle="tooltip" title="Modify this machine">
-                            <button class="btn btn-primary btn-sm" data-toggle="tooltip" title="Edit this machien">
+                            <button class="btn btn-primary btn-sm" data-toggle="tooltip" title="Modify this machine">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
                                 <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
                                 </svg>
@@ -58,5 +58,10 @@
     </tbody>
 </table>
 @if (Auth::check())
-<p><a href="{{ route('tubes.createTubeFor', $machine->id) }}"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add x-ray tube</a></p>
+<p><a href="{{ route('tubes.createTubeFor', $machine->id) }}">
+     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+     <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+     <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+     </svg>
+     Add x-ray tube</a></p>
 @endif
