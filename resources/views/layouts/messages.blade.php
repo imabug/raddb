ef161167844a87a1368af1aa9e2a6190a7a96fe1
Eugene Mah <eugenemah@gmail.com>
2021-07-12 13:32:48 -0400

    Bootstrap5-ify messages widget

--- resources/views/layouts/messages.blade.php
+++ resources/views/layouts/messages.blade.php
@@ -1,11 +1,11 @@
 @if (session('success'))
-    <div class="alert alert-success alert-dismissible">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        {{ session('success') }}
+    <div class="alert alert-success alert-dismissible" role="alert">
+      {{ session('success') }}
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @elseif (session('fail'))
-    <div class="alert alert-danger alert-dismissible">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        {{ session('fail') }}
+    <div class="alert alert-danger alert-dismissible" role="alert">
+      {{ session('fail') }}
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @endif
