94adb324c9316522feda2cebfe59656be54f1709
Eugene Mah <eugenemah@gmail.com>
2021-07-12 11:22:57 -0400

    Bootstrap5-ify Add survey report page

--- resources/views/surveys/surveys_addReport.blade.php
+++ resources/views/surveys/surveys_addReport.blade.php
@@ -6,11 +6,17 @@
 <h2>Add survey report</h2>
 
 <form class="form-inline" action="{{ route('surveyreports.store') }}" method="post" enctype="multipart/form-data">
-    <div class="form-group">
-        {{ csrf_field() }}
-        <p><label for="surveyId">Survey ID: </label> <input class="form-control" type="number" id="surveyId" name="surveyId" list="surveys"></p>
-        <p><label for="surveyReport">Upload survey report: </label> <input class="form-control" type="file" id="surveyReport" name="surveyReport" ></p>
-        <p><button type="submit">Submit survey report</button></p>
+{{ csrf_field() }}
+  <div class="row">
+    <div class="col input-group mb-3">
+      <span class="input-group-text">Survey ID: </span>
+      <input class="form-control" type="number" id="surveyId" name="surveyId" list="surveys" aria-label="Enter machine ID">
     </div>
+    <div class="col input-group mb-3">
+      <span class="input-group-text">Upload survey report: </span>
+      <input class="form-control" type="file" id="surveyReport" name="surveyReport" aria-label="Survey report to upload">
+    </div>
+  </div>
+  <button class="btn btn-primary" type="submit">Submit survey report</button>
 </form>
 @endsection
