77c97bd0fa1345221c0b82cad6a273698b079b63
Eugene Mah <eugenemah@gmail.com>
2021-10-04 09:37:19 -0400

    Remove effort calculation method
    Effort calculation is probably something better done using a spreadsheet.
    Dropping it for now

--- app/Http/Controllers/AnnReportController.php
+++ app/Http/Controllers/AnnReportController.php
@@ -9,39 +9,6 @@ use Illuminate\Http\Request;
 class AnnReportController extends Controller
 {
     /**
-     * Show a summary of the estimated effort for testing all active machines
-     * URI: /ar/effort
-     */
-    public function effort()
-    {
-        // Get the list of all active machines
-        $machines = Machine::with('modality', 'manufacturer', 'location', 'tube')
-            ->active()
-            ->get()
-            ->groupBy('modality.modality');
-
-        foreach ($machines as $key=>$modality) {
-            $n[$key] = count($modality);
-            $tubes[$key] = 0;
-            foreach ($modality as $machine) {
-                echo $machine->description." "." ".$key." ".$machine->tube()->active()->count()."\n";
-                if ($machine->tube()->active()->count() == 0) {
-                    // If there are no tubes recorded in the DB, assume the machine has one tube
-                    $tubes[$key]++;
-                }
-                else {
-                    $tubes[$key] += $machine->tube()->active()->count();
-                }
-                echo $key." ".$tubes[$key]."\n";
-            }
-        }
-        return view('ar.effort', [
-            'n' => $n,
-            'tubes' => $tubes,
-        ]);
-    }
-
-    /**
      * Generate a mammography continued experience report
      * URI: /ar/mamcexp
      */
