b4a9f354bd64e1391a01aeaaba11fa9babdf102d
Eugene Mah <eugenemah@gmail.com>
2021-10-04 13:37:06 -0400

    Table experiment with livewire

---
+++ app/Http/Livewire/Testtables.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace RadDB\Http\Livewire;
+
+use Livewire\Component;
+
+class Testtables extends Component
+{
+    public function render()
+    {
+        return view('livewire.testtables');
+    }
+}
