9dd9dd4145284835c3dddca604f144d1da798737
Eugene Mah <eugenemah@gmail.com>
2021-10-06 12:17:18 -0400

    Update for Laravel 8 routing namespace changes
    https://laravel.com/docs/8.x/releases#routing-namespace-updates

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace RadDB\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
@@ -30,52 +33,29 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Route parameter constraints
-        Route::pattern('id', '[0-9]+');
+        $this->configureRateLimiting();
 
-        parent::boot();
+        $this->routes(function () {
+            Route::prefix('api')
+                ->middleware('api')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/api.php'));
+
+            Route::middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/web.php'));
+        });
     }
 
     /**
-     * Define the routes for the application.
+     * Configure the rate limiters for the application.
      *
      * @return void
      */
-    public function map()
+    protected function configureRateLimiting()
     {
-        $this->mapWebRoutes();
-
-        $this->mapApiRoutes();
-
-        //
-    }
-
-    /**
-     * Define the "web" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     *
-     * @return void
-     */
-    protected function mapWebRoutes()
-    {
-        Route::middleware('web')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/web.php'));
-    }
-
-    /**
-     * Define the "api" routes for the application.
-     *
-     * These routes are typically stateless.
-     *
-     * @return void
-     */
-    protected function mapApiRoutes()
-    {
-        Route::prefix('api')
-             ->middleware('api')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/api.php'));
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,21 +11,42 @@
 |
 */
 
+use RadDB\Http\Controllers\DashboardController;
+use RadDB\Http\Controllers\MachineListingController;
+use RadDB\Http\Controllers\LocationController;
+use RadDB\Http\Controllers\ManufacturerController;
+use RadDB\Http\Controllers\ModalityController;
+use RadDB\Http\Controllers\MachineController;
+use RadDB\Http\Controllers\TestEquipmentController;
+use RadDB\Http\Controllers\OpNoteController;
+use RadDB\Http\Controllers\RecommendationController;
+use RadDB\Http\Controllers\TestDateController;
+use RadDB\Http\Controllers\SurveyReportController;
+use RadDB\Http\Controllers\ServiceReportController;
+use RadDB\Http\Controllers\TubeController;
+use RadDB\Http\Controllers\MachinePhotoController;
+use RadDB\Http\Controllers\UserController;
+use RadDB\Http\Controllers\HomeController;
+use RadDB\Http\Controllers\AnnReportController;
+
+// Livewire components
+use RadDB\Http\Livewire\Testtables;
+
 /*
  * Dashboard routes
  */
-Route::name('index')->get('/', 'DashboardController@index'); // Survey schedule
+Route::name('index')->get('/', [DashboardController::class, 'index']); // Survey schedule
 Route::name('dashboard.dashboard')
-    ->get('/dashboard', 'DashboardController@teststatus');
+    ->get('/dashboard', [DashboardController::class, 'teststatus']);
 // Untested machines
 Route::name('dashboard.showUntested')
-    ->get('/dashboard/showUntested', 'DashboardController@showUntested');
+    ->get('/dashboard/showUntested', [DashboardController::class, 'showUntested']);
 // Pending surveys
 Route::name('dashboard.showPending')
-    ->get('/dashboard/showPending', 'DashboardController@showPending');
+    ->get('/dashboard/showPending', [DashboardController::class, 'showPending']);
 // Survey schedule
 Route::name('dashboard.showSchedule')
-    ->get('/dashboard/showSchedule', 'DashboardController@showSchedule');
+    ->get('/dashboard/showSchedule', [DashboardController::class, 'showSchedule']);
 // Count of surveys per month
 // Route::name('dashboard.surveyGraph')
 //     ->get('/dashboard/surveyGraph', 'DashboardController@surveyGraph');
@@ -35,131 +56,132 @@ Route::name('dashboard.showSchedule')
  */
 // Inactive machines
 Route::name('machines.inactive')
-    ->get('/machines/inactive', 'MachineListingController@showInactive');
+    ->get('/machines/inactive', [MachineListingController::class, 'showInactive']);
 // Removed machines
 Route::name('machines.removed')
-    ->get('/machines/removed/{year?}', 'MachineListingController@showRemoved');
+    ->get('/machines/removed/{year?}', [MachineListingController::class, 'showRemoved']);
 // Installed machines by year
 Route::name('machines.installed')
-    ->get('/machines/installed/{year?}', 'MachineListingController@showInstalled');
+    ->get('/machines/installed/{year?}', [MachineListingController::class, 'showInstalled']);
 // Index of machines grouped by location
 Route::name('machines.showLocationIndex')
-    ->get('/machines/locations', 'LocationController@index');
+    ->get('/machines/locations', [LocationController::class, 'index']);
 // List of machines for a selected location(s)
 Route::name('machines.showLocation')
-    ->get('/machines/locations/{id}', 'LocationController@show');
+    ->get('/machines/locations/{id}', [LocationController::class, 'show']);
 // Index of machines grouped by manufacturer
 Route::name('machines.showManufacturerIndex')
-    ->get('/machines/manufacturers', 'ManufacturerController@index');
+    ->get('/machines/manufacturers', [ManufacturerController::class, 'index']);
 // List of machines for a selected modality/modalities
 Route::name('machines.showManufacturer')
-    ->get('/machines/manufacturers/{id}', 'ManufacturerController@show');
+    ->get('/machines/manufacturers/{id}', [ManufacturerController::class, 'show']);
 // Index of machines grouped by modality
 Route::name('machines.showModalityIndex')
-    ->get('/machines/modalities', 'ModalityController@index');
+    ->get('/machines/modalities', [ModalityController::class, 'index']);
 // List of machines for a selected modality/modalities
 Route::name('machines.showModality')
-    ->get('/machines/modalities/{id}', 'ModalityController@show');
+    ->get('/machines/modalities/{id}', [ModalityController::class, 'show']);
 
 /*
  * Machine controller
  */
-Route::resource('machines', 'MachineController');
+Route::resource('machines', MachineController::class);
 
 /*
  * Test equipment controller
  */
 Route::name('testequipment.showCalDates')
-    ->get('/testequipment/caldates', 'TestEquipmentController@showCalDates');
-Route::resource('testequipment', 'TestEquipmentController');
+    ->get('/testequipment/caldates', [TestEquipmentController::class, 'showCalDates']);
+Route::resource('testequipment', TestEquipmentController::class);
 
 /*
  * Operational notes controller
  */
 Route::name('opnotes.createOpNoteFor')
-    ->get('opnotes/{machineId?}/create', 'OpNoteController@create');
-Route::resource('opnotes', 'OpNoteController');
+    ->get('opnotes/{machineId?}/create', [OpNoteController::class, 'create']);
+Route::resource('opnotes', OpNoteController::class);
 
 /*
  * Recommendation controller
  */
 Route::name('recommendations.createRecFor')
-    ->get('recommendations/{id?}/create', 'RecommendationController@create');
-Route::resource('recommendations', 'RecommendationController');
+    ->get('recommendations/{id?}/create', [RecommendationController::class, 'create']);
+Route::resource('recommendations', RecommendationController::class);
 
 /*
  * Test Date controller
  */
 Route::name('surveys.createSurveyFor')
-    ->get('surveys/{id?}/create', 'TestDateController@create');
-Route::resource('surveys', 'TestDateController');
+    ->get('surveys/{id?}/create', [TestDateController::class, 'create']);
+Route::resource('surveys', TestDateController::class);
 
 /*
  * Survey report controller
  */
 Route::name('surveyreports.create')
-    ->get('surveyreports/{id?}/create', 'SurveyReportController@create');
-Route::resource('surveyreports', 'SurveyReportController');
+    ->get('surveyreports/{id?}/create', [SurveyReportController::class, 'create']);
+Route::resource('surveyreports', SurveyReportController::class,
+                ['except' => ['create']]);
 
 /*
  * Service report controller
  */
-Route::resource('servicereports', 'ServiceReportController');
+Route::resource('servicereports', ServiceReportController::class);
 
 /*
  * Tube controller
  */
-Route::name('tubes.createTubeFor')->get('tubes/{id}/create', 'TubeController@create');
-Route::resource('tubes', 'TubeController');
+Route::name('tubes.createTubeFor')->get('tubes/{id}/create', [TubeController::class, 'create']);
+Route::resource('tubes', TubeController::class);
 
 /*
  * Photos controller. Used to handle uploading and updating photos of machines.
  */
 Route::name('photos.create')
-    ->get('photos/{id}/create', 'MachinePhotoController@create');
-Route::resource('photos', 'MachinePhotoController',
+    ->get('photos/{id}/create', [MachinePhotoController::class, 'create']);
+Route::resource('photos', MachinePhotoController::class,
     ['except' => ['create']]);
 
 /*
  * Route for user management
  */
-Route::resource('users', 'UserController');
+Route::resource('users', UserController::class);
 
 /*
  * Authentication routes
  */
 Auth::routes();
-Route::name('home.index')->get('/home', 'HomeController@index');
+Route::name('home.index')->get('/home', [HomeController::class, 'index']);
 
 /*
  * Routes for viewing QA/survey data
  */
-Route::name('qa.index')->get('qa/', 'QAController@index');
-Route::name('qa.machineSurveyList')->get('qa/{machine_id}/surveyList', 'QAController@show');
+// Route::name('qa.index')->get('qa/', 'QAController@index');
+// Route::name('qa.machineSurveyList')->get('qa/{machine_id}/surveyList', 'QAController@show');
 
 /*
  * Survey test data routes
  */
-Route::name('gendata.create')
-    ->get('surveydata/generator/{surveyId}/create', 'GenDataController@create');
-Route::resource('gendata', 'GenDataController',
-                ['except' => ['create']]);
-Route::resource('collimatordata', 'CollimatorDataController');
-Route::resource('fluorodata', 'FluoroDataController');
-Route::resource('hvldata', 'HVLDataController');
-Route::resource('maxfluorodata', 'MaxFluoroDataController');
-Route::resource('radsurveydata', 'RadSurveyDataController');
-Route::resource('radoutputdata', 'RadiationOutputController');
-Route::resource('receptorentrance', 'ReceptorEntranceExpController');
+// Route::name('gendata.create')
+//     ->get('surveydata/generator/{surveyId}/create', 'GenDataController@create');
+// Route::resource('gendata', 'GenDataController',
+//                 ['except' => ['create']]);
+// Route::resource('collimatordata', 'CollimatorDataController');
+// Route::resource('fluorodata', 'FluoroDataController');
+// Route::resource('hvldata', 'HVLDataController');
+// Route::resource('maxfluorodata', 'MaxFluoroDataController');
+// Route::resource('radsurveydata', 'RadSurveyDataController');
+// Route::resource('radoutputdata', 'RadiationOutputController');
+// Route::resource('receptorentrance', 'ReceptorEntranceExpController');
 
 // CT Daily QC routes
-Route::resource('ctdailyqc', 'CTDailyQCRecordController');
+//Route::resource('ctdailyqc', 'CTDailyQCRecordController');
 
 /*
  * Reporting routes
  */
-Route::name('ar.cexp')->get('/ar/cexp', 'AnnReportController@mammContExp');
-Route::name('ar.annrep')->get('ar/{year}/annrep/', 'AnnReportController@annrep');
+Route::name('ar.cexp')->get('/ar/cexp', [AnnReportController::class, 'mammContExp']);
+Route::name('ar.annrep')->get('ar/{year}/annrep/', [AnnReportController::class, 'annrep']);
 
 // Experimental routes
-Route::name('test.tabletest')->get('/test/tabletest', 'TestController@tabletest');
+Route::name('test.testtables')->get('/test/testtables', Testtables::class);
