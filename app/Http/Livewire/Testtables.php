773434f9b734b8b76a73828c1ebd8e62441d7bab
Eugene Mah <eugenemah@gmail.com>
2021-10-06 12:43:07 -0400

    Livewire testing

--- app/Http/Livewire/Testtables.php
+++ app/Http/Livewire/Testtables.php
@@ -3,12 +3,15 @@
 namespace RadDB\Http\Livewire;
 
 use Livewire\Component;
+use RadDB\Machine;
 
 class Testtables extends Component
 {
     public function render()
     {
-        return view('livewire.testtables')
+        return view('livewire.testtables', [
+            'machines' => Machine::active()->get(),
+        ])
             ->extends('layouts.app')
             ->section('content');
     }
--- resources/views/livewire/testtables.blade.php
+++ resources/views/livewire/testtables.blade.php
@@ -1,3 +1,20 @@
 <div>
-    Hello World
+    <table>
+    <thead>
+    <th>ID</th>
+    <th>Description</th>
+    <th>Model</th>
+    <th>Serial Number</th>
+    </thead>
+    <tbody>
+    @foreach($machines as $m)
+        <tr>
+        <td>{{$m->id}}</td>
+        <td>{{$m->description}}</td>
+        <td>{{$m->model}}</td>
+        <td>{{$m->serial_number}}</td>
+        </tr>
+    @endforeach
+    </tbody>
+    </table>
 </div>
