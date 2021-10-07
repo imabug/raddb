82cd9eea63e177f231306b53cddc12fac2713518
Eugene Mah <eugenemah@gmail.com>
2021-10-07 10:09:38 -0400

    Clean up unused models and controllers
    Signed-off-by: Eugene Mah <eugenemah@gmail.com>

--- resources/views/qa/radoutput/show.blade.php
+++
@@ -1,9 +0,0 @@
-<!-- resources/views/qa/radoutput/show.blade.php -->
-
-@extends('layouts.app')
-
-@section('content')
-    <h2>Radiation output data for {{ $survey->machine->description }} ({{ $survey->test_date }} - Survey ID {{ $survey->id }})</h2>
-{!! Charts::assets(['google']) !!}
-@include('gendata.radOutput')
-@endsection
