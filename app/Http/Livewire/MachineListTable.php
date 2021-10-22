9f2d0ddcf8a192d661767e70fadc3b1dd9045bcd
Eugene Mah <eugenemah@gmail.com>
2021-10-22 13:43:02 -0400

    Get column searching to work

--- app/Http/Livewire/MachineListTable.php
+++ app/Http/Livewire/MachineListTable.php
@@ -2,16 +2,23 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Location;
 use App\Models\Machine;
+use App\Models\Manufacturer;
+use App\Models\Modality;
 use Illuminate\Database\Eloquent\Builder;
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Views\Column;
 
+/**
+ * Livewire datatables component that provides a list of
+ * machines for the machines listing view (/machines/)
+ */
 class MachineListTable extends DataTableComponent
 {
     public string $defaultSortColumn = 'id';
     public string $defaultSortDirection = 'asc';
-    public bool $singleColumnSorting = true;
+    public bool $singleColumnSorting = false;
     public bool $paginationEnabled = false;
 
     public function columns(): array
@@ -19,16 +26,25 @@ class MachineListTable extends DataTableComponent
         return [
             Column::make('ID', 'id'),
             Column::make('Modality', 'modality.modality')
-                ->sortable(),
+                ->sortable(function(Builder $query, $direction) {
+                    return $query
+                        ->orderBy(Modality::select('modality')->whereColumn('id', 'modality_id'), $direction);
+                }),
             Column::make('Manufacturer', 'manufacturer.manufacturer')
-                ->sortable(),
+                ->sortable(function(Builder $query, $direction) {
+                    return $query
+                        ->orderBy(Manufacturer::select('manufacturer')->whereColumn('id', 'manufacturer_id'), $direction);
+                }),
             Column::make('Model', 'model')
                 ->sortable(),
             Column::make('SN','serial_number'),
             Column::make('Description', 'description')
                 ->searchable(),
             Column::make('Location', 'location.location')
-                ->sortable(),
+                ->sortable(function(Builder $query, $direction) {
+                    return $query
+                        ->orderBy(Location::select('location')->whereColumn('id', 'location_id'), $direction);
+                }),
             Column::make('Age', 'age'),
             Column::make('Room', 'room'),
         ];
