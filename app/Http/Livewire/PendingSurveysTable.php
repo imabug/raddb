d9a0cf30403fd97f573367280f6b097430b13aa9
Eugene Mah <eugenemah@gmail.com>
2021-10-29 12:14:15 -0400

    Make some columns sortable

--- app/Http/Livewire/PendingSurveysTable.php
+++ app/Http/Livewire/PendingSurveysTable.php
@@ -9,13 +9,20 @@ use Rappasoft\LaravelLivewireTables\Views\Column;
 
 class PendingSurveysTable extends DataTableComponent
 {
+    public string $defaultSortColumn = 'test_date';
+    public string $defaultSortDirection = 'asc';
+    public bool $singleColumnSorting = false;
+    public bool $paginationEnabled = false;
+    public bool $showSearch = false;
 
     public function columns(): array
     {
         return [
-            Column::make('Survey ID', 'id'),
+            Column::make('Survey ID', 'id')
+                ->sortable(),
             Column::make('Description', 'machine.description'),
-            Column::make('Date scheduled', 'test_date'),
+            Column::make('Date scheduled', 'test_date')
+                ->sortable(),
             Column::make('Test Type', 'type.test_type'),
             Column::make('Accession'),
             Column::make('Survey Note'),
@@ -30,7 +37,6 @@ class PendingSurveysTable extends DataTableComponent
     public function query(): Builder
     {
         return TestDate::with('machine', 'type')
-            ->pending()
-            ->orderBy('testdates.test_date', 'asc');
+            ->pending();
     }
 }
