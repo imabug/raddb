0eb1023c6466ca1d62eaa906077e3127aa5bf6ca
Eugene Mah <eugenemah@gmail.com>
2021-11-10 13:14:54 -0500

    Remove obsolete views

--- resources/views/layouts/errors.blade.php
+++
@@ -1,9 +0,0 @@
-@if (count($errors) > 0)
-<div class="bg-warning">
-    <ul>
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-</div>
-@endif
