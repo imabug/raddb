bcfbde82d14e09749f497f6e9806738de74e428e
Eugene Mah <eugenemah@gmail.com>
2021-11-15 15:17:39 -0500

    Add service provider and alias for khill/lavacharts package

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Khill\Lavacharts\Laravel\LavachartsServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -225,6 +226,7 @@ return [
         'URL'          => Illuminate\Support\Facades\URL::class,
         'Validator'    => Illuminate\Support\Facades\Validator::class,
         'View'         => Illuminate\Support\Facades\View::class,
+        'Lava'         => Khill\Lavacharts\Laravel\LavachartsFacade::class,
     ],
 
 ];
