23f36c6555070b756c38168c99366b65a00013ee
Eugene Mah <eugenemah@gmail.com>
2021-11-15 11:19:35 -0500

    Make the chart bigger and add hook for colours

--- resources/views/dashboard/survey_graph.blade.php
+++ resources/views/dashboard/survey_graph.blade.php
@@ -19,7 +19,7 @@
   --}}
   
   <!-- Chart's container -->
-  <div id="chart" style="height: 300px;"></div>
+  <div id="chart" style="height: 600px;"></div>
   <!-- Charting library -->
   <script src="https://unpkg.com/chart.js@2.9.3/dist/Chart.min.js"></script>
   <!-- Chartisan -->
@@ -29,6 +29,8 @@
    const chart = new Chartisan({
      el: '#chart',
      url: "@chart('survey_graph')",
+     hooks: new ChartisanHooks()
+       .colors(),
    });
   </script>
   
