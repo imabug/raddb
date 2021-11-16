287d6be451a8d1ffa6374f5383600ecd8a20ccd7
Eugene Mah <eugenemah@gmail.com>
2021-11-16 13:04:12 -0500

    Add component for links to service reports

---
+++ app/View/Components/ServiceReportLink.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\View\Components;
+
+use App\Models\TestDate;
+use Illuminate\View\Component;
+
+class ServiceReportLink extends Component
+{
+    /**
+     * Survey ID.
+     *
+     * @int $surveyID
+     */
+    public $surveyID;
+
+    /**
+     * Service report link.
+     *
+     * @string $surveyLink
+     */
+    public $serviceReportLink;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct(?int $surveyId)
+    {
+        $this->surveyId = $surveyId;
+
+        $survey = TestDate::find($this->surveyId);
+
+        if (!is_null($survey) && $survey->hasMedia('service_report')) {
+            $this->serviceReportLink = $survey->getFirstMediaUrl('service_report');
+        } else {
+            $this->serviceReportLink = null;
+        }
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.service-report-link');
+    }
+}
