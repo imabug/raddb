c73e64088ed355ceb865a0dbf5f082f5ba471689
Eugene Mah <eugenemah@gmail.com>
2021-11-16 08:17:48 -0500

    Split up the graph areas with header sections

--- resources/views/dashboard/survey_counts.blade.php
+++ resources/views/dashboard/survey_counts.blade.php
@@ -2,9 +2,10 @@
 @extends('layouts.app')
 @section('content')
   <h2>Survey Count Graphs</h2>
+  <h3>Total surveys by year</h3>
   <div id="perf_div"></div>
   @columnchart('Yearly Survey Count', 'perf_div')
-
+  <h3>Survey count by month</h3>
   @foreach ($years as $y)
     <div id={{ 'perf_div'.$y->years }}></div>
     @columnchart('Survey count - '.$y->years, 'perf_div'.$y->years)
