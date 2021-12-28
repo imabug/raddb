7f6be32ec92d712e1c02ec33dc4d64da5ee071bf
Eugene Mah <eugenemah@gmail.com>
2021-12-28 12:29:42 -0500

    Update tests

--- tests/Feature/DashboardPagesTest.php
+++ tests/Feature/DashboardPagesTest.php
@@ -7,28 +7,6 @@ use Tests\TestCase;
 class DashboardPagesTest extends TestCase
 {
     /**
-     * Test to see that the dashboard routes load.
-     *
-     * @dataProvider dashboardRoutes
-     *
-     * @return void
-     */
-    public function testDashboardRoutesLoadProperly($routeName, $value)
-    {
-        $this->get(route($routeName))->assertSee($value);
-    }
-
-    public function dashboardRoutes(): array
-    {
-        return [
-            'Survey status'           => ['dashboard.dashboard', 'Equipment Testing Status Dashboard'],
-            'Surveys to be scheduled' => ['dashboard.showUntested', 'Surveys to be scheduled'],
-            'Pending survey'          => ['dashboard.showPending', 'Pending surveys'],
-            'Survey schedule'         => ['dashboard.showSchedule', 'Survey Schedule'],
-        ];
-    }
-
-    /**
      * Test to see if the dashboard pages load via HTTP requests.
      *
      * @dataProvider dashboardPages
@@ -44,9 +22,7 @@ class DashboardPagesTest extends TestCase
     {
         return [
             'Survey status'           => ['/dashboard', 'Equipment Testing Status Dashboard'],
-            'Surveys to be scheduled' => ['/dashboard/showUntested', 'Surveys to be scheduled'],
-            'Pending survey'          => ['/dashboard/showPending', 'Pending surveys'],
-            'Survey schedule'         => ['/dashboard/showSchedule', 'Survey Schedule'],
+            'Survey graphs'           => ['dashboard/surveyCount', 'Survey graphs'],
         ];
     }
 }
--- tests/Feature/ListingsTest.php
+++ tests/Feature/ListingsTest.php
@@ -7,32 +7,6 @@ use Tests\TestCase;
 class ListingsTest extends TestCase
 {
     /**
-     * Test to see that the Listings pages load.
-     *
-     * @dataProvider listingRoutes
-     *
-     * @return void
-     */
-    public function testListingRoutesLoadProperly($routeName, $value)
-    {
-        $this->get(route($routeName))->assertSee($value);
-    }
-
-    public function listingRoutes(): array
-    {
-        return [
-            'Machines listing'                 => ['machines.index', 'Equipment Inventory - Active'],
-            'Modality listing'                 => ['machines.showModalityIndex', 'List equipment by modality'],
-            'Location listing'                 => ['machines.showLocationIndex', 'List equipment by location'],
-            'Manufacturer listing'             => ['machines.showManufacturerIndex', 'List equipment by manufacturer'],
-            'Inactive machines listing'        => ['machines.inactive', 'Equipment Inventory - Inactive'],
-            'Removed machines listing'         => ['machines.removed', 'Equipment Inventory - Removed'],
-            'Test equipment listing'           => ['testequipment.index', 'Equipment Inventory - Test Equipment'],
-            'Test equipment cal dates listing' => ['testequipment.showCalDates', 'Recent Test Equipment Calibration Dates'],
-        ];
-    }
-
-    /**
      * Test that the Listings pages load via HTTP requests.
      *
      * @dataProvider ListingPages
@@ -48,11 +22,6 @@ class ListingsTest extends TestCase
     {
         return [
             'Machines listing'                 => ['/machines', 'Equipment Inventory - Active'],
-            'Modality listing'                 => ['/machines/modalities', 'List equipment by modality'],
-            'Location listing'                 => ['/machines/locations', 'List equipment by location'],
-            'Manufacturer listing'             => ['/machines/manufacturers', 'List equipment by manufacturer'],
-            'Inactive machines listing'        => ['/machines/inactive', 'Equipment Inventory - Inactive'],
-            'Removed machines listing'         => ['/machines/removed', 'Equipment Inventory - Removed'],
             'Test equipment listing'           => ['/testequipment', 'Equipment Inventory - Test Equipment'],
             'Test equipment cal dates listing' => ['/testequipment/caldates', 'Recent Test Equipment Calibration Dates'],
         ];
