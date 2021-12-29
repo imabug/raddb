ef9d7bc32d9227078338199c51bcf5e5412d8994
Eugene Mah <eugenemah@gmail.com>
2021-12-29 10:34:33 -0500

    Work on op notes page.
    Livewire part for selecting the machine seems to be working.  Can echo
    the ID of the machine being selected. Just not getting anything
    returned when the Get Notes button is submitted.

--- app/Http/Livewire/Opnotes/OperationalNotes.php
+++ app/Http/Livewire/Opnotes/OperationalNotes.php
@@ -7,9 +7,9 @@ use Livewire\Component;
 
 class OperationalNotes extends Component
 {
-    public $machine = null;
-    public $opNotes = null;
-    public $m = null;
+    public $machine;
+    public $opNotes;
+    public $mNotes;
 
     public function mount()
     {
@@ -18,11 +18,13 @@ class OperationalNotes extends Component
 
     public function getNotes()
     {
-        $this->opNotes = Machine::find($m)->opnote()->get();
+        $this->opNotes = Machine::find($this->mNotes)->opnote()->get();
     }
 
     public function render()
     {
-        return view('livewire.opnotes.operational-notes');
+        return view('livewire.opnotes.operational-notes')
+            ->extends('layouts.app')
+            ->section('content');
     }
 }
--- resources/views/livewire/opnotes/operational-notes.blade.php
+++ resources/views/livewire/opnotes/operational-notes.blade.php
@@ -1,28 +1,24 @@
-@extends('layouts.app')
-
-@section('content')
-  <div class="row">
-    <h2>Operational notes</h2>
-    <div class="col input-group mb-3"
-      <form wire:submit.prevent="getNotes">
-        <span class="input-group-text">Machine:</span>
-        <select wire:model="m">
-          <option>Select a machine</option>
-          @foreach ($machine as $m)
-            <option value="{{$m->id}}">{{$m->description}}</option>
-          @endforeach
-        </select>
-        <button type="submit">Get Notes</button>
-      </form>
-    </div>
-    @if(!is_null($opNotes))
-    <div>
-      <ol>
+<div>
+  <h2>Operational notes</h2>
+  <div class="col input-group mb-3"
+    <form class="form-inline" wire:submit.prevent="getNotes">
+      @csrf
+      <span class="input-group-text">Machine:</span>
+      <select id="mNotes" name="mNotes" class="form-select" aria-label="Select a machine" wire:model="mNotes">
+        <option>Select a machine</option>
+        @foreach ($machine as $m)
+          <option value="{{$m->id}}">{{$m->description}}</option>
+        @endforeach
+      </select>
+      <button>Get Notes</button>
+    </form>
+    <p>{{$mNotes}}</p>
+  </div>
+  @if(!is_null($opNotes))
+    <ol>
       @foreach ($opNotes as $o)
         <li wire:key="{{$o->id}}">$o->note</li>
       @endforeach
-      </ol>
-    </div>
-    @endif
-  </div>
-@endsection
+    </ol>
+  @endif
+</div>
