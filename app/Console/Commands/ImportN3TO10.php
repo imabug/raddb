302ec1707a641d199a349cc7de5db1378cf26082
Eugene Mah <eugenemah@gmail.com>
2022-08-02 13:25:53 -0400

    Load Leeds N3 data
    Loading Leeds N3 data is pretty straight forward.
    Need to think about how to process the TO.10 data.

--- app/Console/Commands/ImportN3TO10.php
+++ app/Console/Commands/ImportN3TO10.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Models\GenData;
 use App\Models\LeedsN3;
+use App\Models\LeedsTO10;
 use App\Models\Machine;
 use App\Models\TestDate;
 use App\Models\Tube;
@@ -46,7 +47,7 @@ class ImportN3TO10 extends Command
 
         $spreadsheet = $reader
             ->setReadDataOnly(true)
-            ->setLoadSheetsOnly(['Gen_form', 'Sheet1'])
+            ->setLoadSheetsOnly(['Fluoro'])
             ->load($surveyFile);
 
         $progressBar->advance();
@@ -56,6 +57,107 @@ class ImportN3TO10 extends Command
             ->getActiveSheet()
             ->getCell('F13')
             ->getCalculatedValue();
+
+        // Check to see if data for this survey ID already exists.
+        if ((LeedsN3::where('survey_id', $surveyId)->get()->count() > 0) &&
+            (LeedsTO10::where('survey_id', $surveyId)->get()->count() > 0)) {
+            // There's already data for this $surveyId present.  Exit
+            $progressBar->finish();
+            $this->newLine();
+            $this->info('Leeds N3 and TO.10 data for this survey already exists.  Exiting');
+            return 1;
+        }
+
+        // Get test date data
+        $testDate = TestDate::with('machine')
+            ->find($surveyId);
+
+        // Get machine information
+        $machine = $testDate->machine;
+
+        // Get tube information.
+        // There will usually be only one tube, but for RF rooms,
+        // need to make sure we get the radiographic tube
+        $tubes = Tube::where('machine_id', $machine->id)->get();
+
+        if ($tubes->count() > 1) {
+            // More than one tube for this machine.
+            // Ask the user which tube should be associated with the generator data
+            $this->newLine();
+            foreach ($tubes as $tube) {
+                $this->line($tube->id.': '.$tube->housing_model.' SN: '.$tube->housing_sn.' '.$tube->notes.' Status:'.$tube->tube_status);
+                $tubeChoice[] = $tube->id;
+            }
+            $tubeId = $this->choice(
+                'Select the tube to associate ',
+                $tubeChoice,
+                $defaultIndex = 0,
+                $maxAttempts = null,
+                $allowMultipleSelections = false
+            );
+        } else {
+            $tubeId = $tubes->first()->id;
+        }
+
+        $progressBar->advance();
+
+        // Get Leeds test data (D401:K405)
+        // Field size is D401:D405
+        // N3 data is J401:J405
+        $leeds = $spreadsheet
+            ->getActiveSheet()
+            ->rangeToArray(
+                'D401:K405',
+                null,
+                true,
+                ture,
+                true
+            );
+        $progressBar->advance();
+
+        /*
+         * Insert N3 data into the database
+         *
+         * D: field size
+         * J: N3 data
+         */
+        foreach ($leeds as $row) {
+            if (~is_numeric($row['D'])) {
+                // No measurement here, so we can skip this row
+                continue;
+            }
+            $n3 = new LeedsN3();
+            $n3->survey_id = $surveyId;
+            $n3->machine_id = $machine->id;
+            $n3->tube_id = $tubeId;
+            $n3->field_size = $row['D'];
+            $n3->n3 = $row['J'];
+            $n3->save();
+            $progressBar->advance();
+        }
+
+        /*
+         * Get Leeds TO10 data (C414:M426)
+         * Contrast detail
+         * Field size: D414-H414
+         * CD data: D415:H426
+         * Threshold index
+         * Field size: I414:M414
+         * TI data: I415:M426
+        */
+        $leeds_to10 = $spreadsheet
+            ->getActiveSheet()
+            ->rangeToArray(
+                'C414:M426',
+                null,
+                true,
+                true,
+                true);
+        $progressBar->advance();
+
+        /*
+         * Insert TO.10 data into the database
+         */
 
         return 0;
     }
