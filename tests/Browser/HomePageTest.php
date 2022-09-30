bdde89bd9cc3709ff04d5b65a8f75c87690afc63
Eugene Mah <eugenemah@gmail.com>
2022-09-30 10:40:42 -0400

    Move HomePage test to Browser folder

---
+++ tests/Browser/HomePageTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use Laravel\Dusk\Browser
+// namespace Tests\Feature;
+
+// use Tests\TestCase;
+
+// class HomePageTest extends TestCase
+// {
+//     /**
+//      * A basic feature test example.
+//      *
+//      * @return void
+//      */
+//     public function testIndexPageLoadsViaHttp()
+//     {
+//         $this->get('/')->assertStatus(200);
+//     }
+
+//     /**
+//      * Test index route.
+//      *
+//      * @return void
+//      */
+//     public function testIndexRouteLoads()
+//     {
+//         $this->get(route('index'))->assertSee('Radiological Equipment Database');
+//     }
+// }
