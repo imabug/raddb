bdc48cfde5b8f8490e3c670c4cc952122be3920b
Eugene Mah <eugenemah@gmail.com>
2022-09-30 10:02:45 -0400

    Rewrite Dashboard pages test as Pest PHP test

--- tests/Feature/DashboardPagesTest.php
+++ tests/Feature/DashboardPagesTest.php
@@ -1,28 +1,42 @@
 <?php
 
-namespace Tests\Feature;
+test('Survey status page exists', function () {
+    $response = $this->get('/dashboard');
+    $response->assertStatus(200);
+});
 
-use Tests\TestCase;
+test('Survey graph page exists', function () {
+    $response = $this->get('/dashboard/surveyCount');
+    $response->assertStatus(200);
+});
 
-class DashboardPagesTest extends TestCase
-{
-    /**
-     * Test to see if the dashboard pages load via HTTP requests.
-     *
-     * @dataProvider dashboardPages
-     *
-     * @return void
-     */
-    public function testDashboardPagesLoadViaHttp($pageName, $value)
-    {
-        $this->get($pageName)->assertStatus(200);
-    }
+test('Survey calendar page exists', function () {
+    $response = $this->get('/dashboard/surveyCalendar');
+    $response->assertStatus(200);
+});
+// namespace Tests\Feature;
 
-    public function dashboardPages(): array
-    {
-        return [
-            'Survey status'           => ['/dashboard', 'Equipment Testing Status Dashboard'],
-            'Survey graphs'           => ['dashboard/surveyCount', 'Survey graphs'],
-        ];
-    }
-}
+// use Tests\TestCase;
+
+// class DashboardPagesTest extends TestCase
+// {
+//     /**
+//      * Test to see if the dashboard pages load via HTTP requests.
+//      *
+//      * @dataProvider dashboardPages
+//      *
+//      * @return void
+//      */
+//     public function testDashboardPagesLoadViaHttp($pageName, $value)
+//     {
+//         $this->get($pageName)->assertStatus(200);
+//     }
+
+//     public function dashboardPages(): array
+//     {
+//         return [
+//             'Survey status'           => ['/dashboard', 'Equipment Testing Status Dashboard'],
+//             'Survey graphs'           => ['dashboard/surveyCount', 'Survey graphs'],
+//         ];
+//     }
+// }
