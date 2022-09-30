ab149b011561c7e6c83c31bce4ee6e8738bdba0a
Eugene Mah <eugenemah@gmail.com>
2022-09-30 10:32:35 -0400

    Rewrite as a Pest PHP test

--- tests/Feature/ListingsTest.php
+++ tests/Feature/ListingsTest.php
@@ -1,29 +1,38 @@
 <?php
 
-namespace Tests\Feature;
+use function Pest\Laravel\get;
 
-use Tests\TestCase;
+test('Lising page exists', function($url) {
+    get($url)->assertStatus(200);
+})->with([
+    'Machines listing' => '/machines',
+    'Test equipment listing' => '/testequipment',
+    'Test equipment calibration dates liisting' => '/testequipment/caldates',
+]);
+// namespace Tests\Feature;
 
-class ListingsTest extends TestCase
-{
-    /**
-     * Test that the Listings pages load via HTTP requests.
-     *
-     * @dataProvider ListingPages
-     *
-     * @return void
-     **/
-    public function testListingPagesLoadViaHttp($pageName, $value)
-    {
-        $this->get($pageName)->assertStatus(200);
-    }
+// use Tests\TestCase;
 
-    public function listingPages(): array
-    {
-        return [
-            'Machines listing'                 => ['/machines', 'Equipment Inventory - Active'],
-            'Test equipment listing'           => ['/testequipment', 'Equipment Inventory - Test Equipment'],
-            'Test equipment cal dates listing' => ['/testequipment/caldates', 'Recent Test Equipment Calibration Dates'],
-        ];
-    }
-}
+// class ListingsTest extends TestCase
+// {
+//     /**
+//      * Test that the Listings pages load via HTTP requests.
+//      *
+//      * @dataProvider ListingPages
+//      *
+//      * @return void
+//      **/
+//     public function testListingPagesLoadViaHttp($pageName, $value)
+//     {
+//         $this->get($pageName)->assertStatus(200);
+//     }
+
+//     public function listingPages(): array
+//     {
+//         return [
+//             'Machines listing'                 => ['/machines', 'Equipment Inventory - Active'],
+//             'Test equipment listing'           => ['/testequipment', 'Equipment Inventory - Test Equipment'],
+//             'Test equipment cal dates listing' => ['/testequipment/caldates', 'Recent Test Equipment Calibration Dates'],
+//         ];
+//     }
+//}
