df4d0afe229e8f19df6766aaeba0e1f37ef887f1
Eugene Mah <eugenemah@gmail.com>
2022-11-09 14:59:22 -0500

    Clean up update and store methods

--- app/Http/Controllers/TestDateController.php
+++ app/Http/Controllers/TestDateController.php
@@ -78,18 +78,10 @@ class TestDateController extends Controller
         $testdate->test_date = $request->test_date;
         $testdate->machine_id = $request->machineID;
         $testdate->tester1_id = $request->tester1ID;
-        if (empty($request->tester2ID)) {
-            $testdate->tester2_id = null;
-        } else {
-            $testdate->tester2_id = $request->tester2ID;
-        }
+        $testdate->tester2_id = $request->has('tester2ID') ? $request->tester2_id : null;
         $testdate->type_id = $request->test_type;
-        if (!empty($request->notes)) {
-            $testdate->notes = $request->notes;
-        }
-        if (!empty($request->accession)) {
-            $testdate->accession = $request->accession;
-        }
+        $testdate->notes = $request->has('notes') ? $request->notes : null;
+        $testdate->accession = $request->has('accession') ? $request->accession : null;
 
         if ($testdate->save()) {
             $status = 'success';
@@ -164,11 +156,7 @@ class TestDateController extends Controller
 
         $testdate->test_date = $request->test_date;
         $testdate->tester1_id = $request->tester1ID;
-        if (empty($request->tester2ID)) {
-            $testdate->tester2_id = null;
-        } else {
-            $testdate->tester2_id = $request->tester2ID;
-        }
+        $testdate->tester2_id = $request->has('tester2ID') ? $request->tester2ID : null;
         $testdate->notes = $request->notes;
         $testdate->accession = $request->accession;
         $testdate->type_id = $request->test_type;
@@ -205,10 +193,9 @@ class TestDateController extends Controller
 
         $message = '';
 
-        $testdate = TestDate::findOrFail($surveyId);
-        if ($testdate->delete()) {
+        if (TestDate::findOrFail($surveyId)->delete()) {
             $status = 'success';
-            $message .= 'Survey '.$testdate->id.' canceled.';
+            $message .= 'Survey '.$surveyId.' canceled.';
             Log::info($message);
         } else {
             $status = 'fail';
