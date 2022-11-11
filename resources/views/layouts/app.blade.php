687c98feb1e27c0b3c77744ea30aba6de2ef1e09
Eugene Mah <eugenemah@gmail.com>
2022-11-10 19:47:18 -0500

    Update jquery, bootstrap versions

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,21 +36,19 @@
 
     <!-- JQuery -->
     <script
-      src="https://code.jquery.com/jquery-3.6.0.min.js"
-      integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+      src="https://code.jquery.com/jquery-3.6.1.min.js"
+      integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ="
       crossorigin="anonymous"></script>
     <script
-      src="https://code.jquery.com/jquery-migrate-3.3.2.min.js"
-      integrity="sha256-Ap4KLoCf1rXb52q+i3p0k2vjBsmownyBTE1EqlRiMwA="
+      src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"
+      integrity="sha256-lSjKY0/srUM9BE3dPm+c4fBo1dky2v27Gdjm2uoZaL0="
       crossorigin="anonymous"></script>
-    <script
-      src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
-      integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
-      crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-migrate-3.4.0.js"></script>
     <!-- Bootstrap core JavaScript -->
+    <!-- JavaScript Bundle with Popper -->
     <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
       crossorigin="anonymous"></script>
     <!-- Alpine.js -->
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
@@ -67,9 +65,6 @@
         src="{{ asset('js/slick_cfg.js') }}"></script>
     @endif
 
-    <!-- Livewire JS -->
-    @livewireScripts
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -82,6 +77,9 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
   </head>
   <body>
+    <!-- Livewire JS -->
+    @livewireScripts
+
     <div class="container">
       <div class="row">
         <div class="col-xs-12 col-md-12">
