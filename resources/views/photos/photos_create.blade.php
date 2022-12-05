5ce84a6fee170a72c9437f30dd508a671c5e6995
Eugene Mah <eugenemah@gmail.com>
2022-12-05 06:30:14 -0500

    Adjust indentation

--- resources/views/photos/photos_create.blade.php
+++ resources/views/photos/photos_create.blade.php
@@ -3,32 +3,32 @@
 @extends('layouts.app')
 
 @section('content')
-<h2>Add photo for {{ $machine->description }}</h2>
-@if($photos->count() > 0)
-<table class="table table-bordered table-condensed">
-    <tbody>
-@foreach ($photos->chunk(4) as $photo_chunk)
-        <tr>
-    @foreach ($photo_chunk as $photo)
-            <td>
+  <h2>Add photo for {{ $machine->description }}</h2>
+  @if($photos->count() > 0)
+    <table class="table table-bordered table-condensed">
+      <tbody>
+        @foreach ($photos->chunk(4) as $photo_chunk)
+          <tr>
+            @foreach ($photo_chunk as $photo)
+              <td>
                 <a href="{{ Storage::disk('MachinePhotos')->url($photo->machine_photo_path) }}" target="_blank">
-                    <img src="{{ Storage::disk('MachinePhotos')->url($photo->machine_photo_path)}}" alt="{{ $photo->photo_description }}" width="150">
+                  <img src="{{ Storage::disk('MachinePhotos')->url($photo->machine_photo_path)}}" alt="{{ $photo->photo_description }}" width="150">
                 </a>
-            </td>
-    @endforeach
-        </tr>
-@endforeach
-    </tbody>
-</table>
-@endif
-<form class="form-inline" action="{{ route('photos.store') }}" method="post" enctype="multipart/form-data">
+              </td>
+            @endforeach
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  @endif
+  <form class="form-inline" action="{{ route('photos.store') }}" method="post" enctype="multipart/form-data">
     <div class="form-group">
-        @csrf
-        <input class="form-control" type="hidden" name="machineId" value="{{ $machine->id }}">
-        <p><label for="photoDescription">Photo description: </label><input class="form-control" type="text" id="photoDescription" name="photoDescription"></p>
-        <p><label for="photo">Upload photo: </label> <input class="form-control" type="file" id="photo" name="photo" ></p>
-        <p><button class="form-control" type="submit">Add photo</button></p>
+      @csrf
+      <input class="form-control" type="hidden" name="machineId" value="{{ $machine->id }}">
+      <p><label for="photoDescription">Photo description: </label><input class="form-control" type="text" id="photoDescription" name="photoDescription"></p>
+      <p><label for="photo">Upload photo: </label> <input class="form-control" type="file" id="photo" name="photo" ></p>
+      <p><button class="form-control" type="submit">Add photo</button></p>
     </div>
-</form>
+  </form>
 
 @endsection
