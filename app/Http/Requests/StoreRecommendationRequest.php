3c22bc4b6d7fd3305a01f76f315092cc67435d49
Eugene Mah <eugenemah@gmail.com>
2022-12-22 10:25:19 -0500

    Add array element types to docblock

--- app/Http/Requests/StoreRecommendationRequest.php
+++ app/Http/Requests/StoreRecommendationRequest.php
@@ -20,7 +20,7 @@ class StoreRecommendationRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array
+     * @return array<string, string>
      */
     public function rules(Request $request)
     {
