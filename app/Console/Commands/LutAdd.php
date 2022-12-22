fbefea9dcd6915c8d290abce8ae73dc6a856d2e4
Eugene Mah <eugenemah@gmail.com>
2022-12-22 13:39:01 -0500

    Remove validation code

--- app/Console/Commands/LutAdd.php
+++ app/Console/Commands/LutAdd.php
@@ -9,8 +9,6 @@ use App\Models\Tester;
 use App\Models\TestType;
 use Illuminate\Console\Command;
 
-//use Illuminate\Support\Facades\Validator;
-
 class LutAdd extends Command
 {
     /**
@@ -52,51 +50,32 @@ class LutAdd extends Command
             case 'location':
                 $lut = new Location();
                 $lut->location = $this->ask('Enter a new location');
-                // $validator = Validator::make($lut, [
-                //     'location' => 'required|string', ]);
                 break;
             case 'manufacturer':
                 $lut = new Manufacturer();
                 $lut->manufacturer = $this->ask('Enter a new manufacturer');
-                // $validator = Validator::make($lut, [
-                //     'manufacturer' => 'required|string', ]);
                 break;
             case 'modality':
                 $lut = new Modality();
                 $lut->modality = $this->ask('Enter a new modality');
-                // $validator = Validator::make($lut, [
-                //     'modality' => 'required|string', ]);
                 break;
             case 'tester':
                 $lut = new Tester();
                 $lut->name = $this->ask('Enter new tester\'s name');
                 $lut->initials = $this->ask('Enter tester\'s initials');
-                // $validator = Validator::make($lut, [
-                //     'name'     => 'required|string',
-                //     'initials' => 'string', ]);
                 break;
             case 'testtype':
                 $lut = new TestType();
                 $lut->test_type = $this->ask('Enter new test type');
-                // $validator = Validator::make($lut, [
-                //     'test_type' => 'required|string|max:4', ]);
                 break;
             default:
                 $this->error('Usage: php artisan lut:add <table>');
                 break;
         }
 
-        // Perform some validation
-        // if ($validator->fails()) {
-        //     $this->error('There were problems with the '.$table.' values provided.');
-
-        //     return 0;
-        // }
-
         if (is_object($lut)) {
             $lut->save();
             $this->info('New '.$table.' entry saved.');
         }
-
     }
 }
