b37d4e4da69b09227be38b88b978ba253106f9ab
Eugene Mah <eugenemah@gmail.com>
2023-08-09 12:02:31 -0400

    Remove policy mappings
    Policy auto-discovery (https://laravel.com/docs/10.x/authorization#registering-policies)
    makes the policy mappings array redundant

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,26 +2,6 @@
 
 namespace App\Providers;
 
-use App\Models\Location;
-use App\Models\Machine;
-use App\Models\Manufacturer;
-use App\Models\Modality;
-use App\Models\OpNote;
-use App\Models\Recommendation;
-use App\Models\TestDate;
-use App\Models\Tester;
-use App\Models\TestType;
-use App\Models\Tube;
-use App\Policies\LocationPolicy;
-use App\Policies\MachinePolicy;
-use App\Policies\ManufacturerPolicy;
-use App\Policies\ModalityPolicy;
-use App\Policies\OpNotePolicy;
-use App\Policies\RecommendationPolicy;
-use App\Policies\TestDatePolicy;
-use App\Policies\TesterPolicy;
-use App\Policies\TestTypePolicy;
-use App\Policies\TubePolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -33,16 +13,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         //'App\Models\Model'         => 'App\Policies\ModelPolicy',
-        Location::class       => LocationPolicy::class,
-        Machine::class        => MachinePolicy::class,
-        Manufacturer::class   => ManufacturerPolicy::class,
-        Modality::class       => ModalityPolicy::class,
-        OpNote::class         => OpNotePolicy::class,
-        Recommendation::class => RecommendationPolicy::class,
-        TestDate::class       => TestDatePolicy::class,
-        Tester::class         => TesterPolicy::class,
-        TestType::class       => TestTypePolicy::class,
-        Tube::class           => TubePolicy::class,
     ];
 
     /**
@@ -52,6 +22,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->registerPolicies();
+        //
     }
 }
