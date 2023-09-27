0ce32d71b3ed0d80c9f977e07d9681436a125665
Eugene Mah <eugenemah@gmail.com>
2023-09-27 14:07:48 -0400

    Start work on machine editing command

--- app/Console/Commands/MachineEdit.php
+++ app/Console/Commands/MachineEdit.php
@@ -2,7 +2,18 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Location;
+use App\Models\Machine;
+use App\Models\Manufacturer;
+use App\Models\Modality;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Validator;
+
+use function Laravel\Prompts\confirm;
+use function Laravel\Prompts\error;
+use function Laravel\Prompts\info;
+use function Laravel\Prompts\multiselect;
+use function Laravel\Prompts\text;
 
 class MachineEdit extends Command
 {
@@ -11,14 +22,14 @@ class MachineEdit extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'machine:edit {machine_id? : ID of the machine to edit}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Edit the details of a machine.  If a machine ID is not provided, a search dialog will be provided to search for a machine to edit';
 
     /**
      * Create a new command instance.
