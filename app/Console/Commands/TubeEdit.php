625d576c9760821cdf566e79c7b32e6d5ba88e6b
Eugene Mah <eugenemah@gmail.com>
2023-09-29 14:01:45 -0400

    Start work on tube edit command

--- app/Console/Commands/TubeEdit.php
+++ app/Console/Commands/TubeEdit.php
@@ -2,7 +2,18 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Machine;
+use App\Models\Manufacturer;
+use App\Models\Tube;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Validator;
+
+use function Laravel\Prompts\confirm;
+use function Laravel\Prompts\error;
+use function Laravel\Prompts\info;
+use function Laravel\Prompts\search;
+use function Laravel\Prompts\select;
+use function Laravel\Prompts\text;
 
 class TubeEdit extends Command
 {
@@ -11,14 +22,14 @@ class TubeEdit extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'tube:edit {machine_id? : ID of the machine to edit the tube(s) for}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Edit the details of an x-ray tube.  If a machine ID is not provided, a search dialog will be provided to search for a machine to edit the tube(s) for.';
 
     /**
      * Create a new command instance.
@@ -33,10 +44,32 @@ class TubeEdit extends Command
     /**
      * Execute the console command.
      *
-     * @return mixed
+     * @return int
      */
-    public function handle()
+    public function handle(): int
     {
-        //
+        $machine = new Machine();
+        $tube = new Tube();
+
+        // Check if a machine ID was provided as an argument.  If not, ask the user to select a machine
+        if (is_null($this->argument('machine_id'))) {
+            $machine = Machine::find(
+                search(
+                    'Search for the machine description to edit',
+                    fn (string $value) => strlen($value) > 0
+                        ? Machine::active()->where('description', 'like', "%{$value}%")->orderBy('description')->pluck('description', 'id')->all()
+                        : []
+                )
+            );
+        } else {
+            $machine = Machine::findOrFail($this->argument('machine_id'));
+        }
+
+        $tube = Tube::active()->forMachine($machine->id)->get();
+
+        foreach ($tube as $t) {
+
+        }
+        return 0;
     }
 }
