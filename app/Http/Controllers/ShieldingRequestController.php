3c60029466cb1920446c57dcd45f7dcbaf86313d
Eugene Mah <eugenemah@gmail.com>
2026-05-08 09:48:24 -0400

    Start work on shielding requests list

---
+++ app/Http/Controllers/ShieldingRequestController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreShieldingRequestRequest;
+use App\Http\Requests\UpdateShieldingRequestRequest;
+use App\Models\ShieldingRequest;
+
+class ShieldingRequestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreShieldingRequestRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ShieldingRequest $shieldingRequest)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ShieldingRequest $shieldingRequest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateShieldingRequestRequest $request, ShieldingRequest $shieldingRequest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ShieldingRequest $shieldingRequest)
+    {
+        //
+    }
+}
